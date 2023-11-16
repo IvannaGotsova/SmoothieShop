92ec31680e0fd959fc928123415de658874912e5
IvannaGotsova <ivannagotsova@outlook.com>
2023-11-17 00:04:13 +0200

    Added Order class constants

--- SmoothieShop.Data/Data/Entites/Order.cs
+++ SmoothieShop.Data/Data/Entites/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,8 @@ namespace SmoothieShop.Data.Data.Entites
         public int OrderId { get; set; }
         public int CustomerId { get; set; }
         public List<int> Smoothies { get; set; } = new List<int>();
+        [Required]
+        [Range(typeof(double), "0.00", "10000.00", ConvertValueInInvariantCulture = true)]
         public decimal Price{ get; set; }
         public DateTime Date { get; set; }
     }
