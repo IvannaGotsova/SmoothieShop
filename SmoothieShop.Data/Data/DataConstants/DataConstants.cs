574b1171d01bf31f3b7986f36fab05f19e6fe5e3
IvannaGotsova <ivannagotsova@outlook.com>
2023-11-16 20:27:46 +0200

    Added Customer class constants

--- SmoothieShop.Data/Data/DataConstants/DataConstants.cs
+++ SmoothieShop.Data/Data/DataConstants/DataConstants.cs
@@ -19,5 +19,17 @@ namespace SmoothieShop.Data.Data.DataConstants
             public const int ApplicationUserMinLengthName = 2;
             public const int ApplicationUserMaxLengthName = 100;
         }
+
+        public class CustomerConstants
+        {
+            public const int CustomerMinLengthName = 2;
+            public const int CustomerMaxLengthName = 100;
+            public const int CustomerMinLengthEmail = 2;
+            public const int CustomerMaxLengthEmail = 100;
+            public const int CustomerMinLengthAddress = 2;
+            public const int CustomerMaxLengthAddress = 100;
+            public const int CustomerMinLengthPhoneNumber = 2;
+            public const int CustomerMaxLengthPhoneNumber = 100;
+        }
     }
 }
--- SmoothieShop.Data/Data/Entites/Customer.cs
+++ SmoothieShop.Data/Data/Entites/Customer.cs
@@ -1,18 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static SmoothieShop.Data.Data.DataConstants.DataConstants.CustomerConstants;
 
 namespace SmoothieShop.Data.Data.Entites
 {
     public class Customer
     {
         public int CustomerId { get; set; }
+        [Required]
+        [StringLength(CustomerMaxLengthName)]
         public string? FirstName { get; set; }
+        [Required]
+        [StringLength(CustomerMaxLengthName)]
         public string? LastName { get; set; }
+        [Required]
+        [StringLength(CustomerMaxLengthEmail)]
         public string? Email { get; set; }
+        [Required]
+        [StringLength(CustomerMaxLengthPhoneNumber)]
         public string? PhoneNumber { get; set; }
+        [Required]
+        [StringLength(CustomerMaxLengthAddress)]
         public string? Address { get; set; }
 
     }
