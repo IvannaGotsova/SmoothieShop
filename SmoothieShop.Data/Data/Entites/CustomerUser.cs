8a4d97d10e394e40458d3ef5927bf7cd3cb8c7e3
IvannaGotsova <ivannagotsova@outlook.com>
2023-11-17 00:15:59 +0200

    Updated ProductUser and CustomerUser classes

--- SmoothieShop.Data/Data/Entites/CustomerUser.cs
+++ SmoothieShop.Data/Data/Entites/CustomerUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,9 @@ namespace SmoothieShop.Data.Data.Entites
 {
     public class CustomerUser : ApplicationUser
     {
+        [Required]
+        public int CustomerUserId { get; set; }
+        [Required]
+        public string ApplicationUserId { get; set; } = null!;
     }
 }
--- SmoothieShop.Data/Data/Entites/ProductUser.cs
+++ SmoothieShop.Data/Data/Entites/ProductUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,9 @@ namespace SmoothieShop.Data.Data.Entites
 {
     public class ProductUser : ApplicationUser
     {
+        [Required]
+        public int ProductUserId { get; set; }
+        [Required]
+        public string ApplicationUserId { get; set; } = null!;
     }
 }
