08edd18cd762b223f3e0b9e805e8241668a84778
IvannaGotsova <ivannagotsova@outlook.com>
2023-11-18 18:42:20 +0200

    Create Configuration classes

---
+++ SmoothieShop.Data/Seeds/ApplicationUserConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SmoothieShop.Data.Data.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmoothieShop.Data.Seeds
+{
+    internal class ApplicationUserConfiguration
+    { 
+    }
+}
+
---
+++ SmoothieShop.Data/Seeds/CustomerConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SmoothieShop.Data.Data.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmoothieShop.Data.Seeds
+{
+    internal class CustomerConfiguration
+    { 
+    }
+}
