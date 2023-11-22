20a6a50f1284c7be0bd62e3419a6f894098310bd
IvannaGotsova <ivannagotsova@outlook.com>
2023-11-22 10:56:06 +0200

    Created ProductUser Controller

---
+++ SmoothieShop/Controllers/ProductUserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmoothieShop.Controllers
+{
+    public class ProductUserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
