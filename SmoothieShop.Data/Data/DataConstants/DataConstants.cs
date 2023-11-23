e25f2f5bb1569ba6db15e024e125babd62acfbee
IvannaGTest <ivannag@students.softuni.bg>
2023-11-23 22:48:34 +0200

    Updated Index View of Home Controller of Admin area

--- SmoothieShop.Data/Data/DataConstants/DataConstants.cs
+++ SmoothieShop.Data/Data/DataConstants/DataConstants.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SmoothieShop.Data.Data.DataConstants
 {
-    public  class DataConstants
+    public class DataConstants
     {
         public class SmoothieConstants
         {
