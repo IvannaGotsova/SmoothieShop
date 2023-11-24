affe82f7f585e38912368bfb21eb603decb1d53a
IvannaGotsova <ivannagotsova@outlook.com>
2023-11-24 22:17:41 +0200

    Sync

--- SmoothieShop.Data/Data/DataConstants/DataConstants.cs
+++ SmoothieShop.Data/Data/DataConstants/DataConstants.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SmoothieShop.Data.Data.DataConstants
 {
-    public static class DataConstants
+    public static class DataConstants      
     {
         public class SmoothieConstants
         {
