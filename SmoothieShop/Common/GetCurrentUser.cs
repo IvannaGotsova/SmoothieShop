8492aebd00d92f3d42b2ea6d8c07c2ad4c6609d3
IvannaGotsova <ivannagotsova@outlook.com>
2023-11-25 00:04:20 +0200

    Created Common folder with GetCurrentUser class

---
+++ SmoothieShop/Common/GetCurrentUser.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+
+namespace SmoothieShop.Common
+{
+    public static class GetCurrentUser
+    {
+        public static string GetCurrentUserId(this ClaimsPrincipal claimsPrincipalUser)
+        {
+            return claimsPrincipalUser.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+        
+        public static string GetCurrentUserName(this ClaimsPrincipal claimsPrincipalUser)
+        {
+            return claimsPrincipalUser.FindFirstValue(ClaimTypes.Name);
+        }
+    }
+}
