cb9f279ea86cf746d87cd1576ad63feb83140ef9
IvannaGotsova <ivannagotsova@outlook.com>
2023-11-25 23:26:25 +0200

    Updated Menu Entity

--- SmoothieShop.Data/Data/Entites/Menu.cs
+++ SmoothieShop.Data/Data/Entites/Menu.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace SmoothieShop.Data.Data.Entites
 {
     public class Menu
     {
+        [Required]
+        public int MenuId { get; set; }
         public IEnumerable<Smoothie> Smoothies { get; set; } = new List<Smoothie>();
     }
 }
