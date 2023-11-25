7a31fbbeffdaede91219ccee95b595d4b6a9a6d9
IvannaGotsova <ivannagotsova@outlook.com>
2023-11-25 23:31:25 +0200

    Updated Order Entity

--- SmoothieShop.Data/Data/Entites/Order.cs
+++ SmoothieShop.Data/Data/Entites/Order.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,12 +12,14 @@ namespace SmoothieShop.Data.Data.Entites
     {
         [Required]
         public int OrderId { get; set; }
-        [Required]
-        public int CustomerId { get; set; }
-        public List<int> Smoothies { get; set; } = new List<int>();
+        public List<Smoothie> Smoothies { get; set; } = new List<Smoothie>();
         [Required]
         [Range(typeof(double), "0.00", "10000.00", ConvertValueInInvariantCulture = true)]
         public decimal Price{ get; set; }
         public DateTime Date { get; set; }
+        [Required]
+        public int CustomerId { get; set; }
+        [ForeignKey(nameof(CustomerId))]
+        public Customer? Customer { get; set; }
     }
 }
