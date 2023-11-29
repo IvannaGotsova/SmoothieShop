10042ebee0111a706ca82164bbe360cb08b851dd
IvannaGotsova <ivannagotsova@outlook.com>
2023-11-29 23:26:52 +0200

    Added summary for IApplicationUserService

--- SmoothieShop.Core/Contracts/IApplicationUserService.cs
+++ SmoothieShop.Core/Contracts/IApplicationUserService.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace SmoothieShop.Core.Contracts
 {
+    /// <summary>
+    /// Holds Interface for Application user functionality.
+    /// </summary>
     public interface IApplicationUserService
     {
     }
