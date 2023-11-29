8cfca725dccfdff0e98482be4b894b2e5c61a783
IvannaGotsova <ivannagotsova@outlook.com>
2023-11-29 23:31:32 +0200

    Added summary for ProductUser interface

--- SmoothieShop.Core/Contracts/IProductUserService.cs
+++ SmoothieShop.Core/Contracts/IProductUserService.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace SmoothieShop.Core.Contracts
 {
+    /// <summary>
+    /// Holds Interface for ProductUser functionality.
+    /// </summary>
     public interface IProductUserService
     {
     }
