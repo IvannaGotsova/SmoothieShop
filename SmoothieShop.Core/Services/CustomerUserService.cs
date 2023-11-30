8ac2ad4559949215c9a88fa45b047cffd36ba9cf
IvannaGotsova <ivannagotsova@outlook.com>
2023-11-30 22:22:23 +0200

    Added summary CustomerUser Service

--- SmoothieShop.Core/Services/CustomerUserService.cs
+++ SmoothieShop.Core/Services/CustomerUserService.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 
 namespace SmoothieShop.Core.Services
 {
+    /// <summary>
+    /// Holds Service for CustomerUser functionality.
+    /// </summary>
     public class CustomerUsserService : ICustomerUserService
     {
     }
