d58aee9014c93543e870675b2104ae2c8e2696e2
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-02 18:42:46 +0200

    Added summary for ApplicationRole

--- SmoothieShop.Data/Data/Entites/ApplicationRole.cs
+++ SmoothieShop.Data/Data/Entites/ApplicationRole.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 
 namespace SmoothieShop.Data.Data.Entites
 {
+    /// <summary>
+    /// Holds ApplicationRole class.
+    /// </summary>
     public class ApplicationRole : IdentityRole
     {
 
