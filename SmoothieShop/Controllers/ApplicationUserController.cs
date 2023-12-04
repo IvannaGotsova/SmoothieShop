ba4dc5c370ffa5390237a2e066343b29eacb46ee
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-04 22:00:19 +0200

    Updated ApllicationUser Controller

--- SmoothieShop/Controllers/ApplicationUserController.cs
+++ SmoothieShop/Controllers/ApplicationUserController.cs
@@ -1,9 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SmoothieShop.Data.Data.Entites;
 
 namespace SmoothieShop.Controllers
 {
     public class ApplicationUserController : Controller
     {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public ApplicationUserController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
         public IActionResult Index()
         {
             return View();
