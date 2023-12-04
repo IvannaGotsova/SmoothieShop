18d9b1057038a277e616b9f5de792bbbb16ea0d4
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-04 22:20:53 +0200

    Created RegisterModelView model

---
+++ SmoothieShop.Data/Models/ApplicationUserModels/RegisterModelView.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static SmoothieShop.Data.Data.DataConstants.DataConstants.ApplicationUserConstants;
+
+namespace SmoothieShop.Models.AplplicationUserModels
+{
+    public class RegisterModelView
+    {
+        [Required]
+        [StringLength(ApplicationUserMaxLengthName, MinimumLength = ApplicationUserMinLengthName)]
+        public string UserName { get; set; } = null!;
+        [Required]
+        [StringLength(ApplicationUserMaxLengthName, MinimumLength = ApplicationUserMinLengthName)]
+        public string FirstName { get; set; } = null!;
+        [Required]
+        [StringLength(ApplicationUserMaxLengthName, MinimumLength = ApplicationUserMinLengthName)]
+        public string LastName { get; set; } = null!;
+        [Required]
+        [EmailAddress]
+        [StringLength(ApplicationUserMaxLengthEmail, MinimumLength = ApplicationUserMinLengthEmail)]
+        public string Email { get; set; } = null!;
+        [Required]
+        [StringLength(ApplicationUserMaxLengthPassword, MinimumLength = ApplicationUserMinLengthPassword)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+        [Required]
+        [Compare(nameof(Password))]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
