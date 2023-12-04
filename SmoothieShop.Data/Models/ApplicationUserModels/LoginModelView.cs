16288ad57414065a01890b7de34c611ad5b5d035
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-04 22:25:31 +0200

    Added LoginModelView model

---
+++ SmoothieShop.Data/Models/ApplicationUserModels/LoginModelView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmoothieShop.Data.Models.ApplicationUserModels
+{
+    public class LoginModelView
+    {
+        [Required]
+        public string UserName { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+    }
+}
