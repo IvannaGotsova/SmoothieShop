cbf5fba4968caefd39f37382020f6c23ea749a19
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-08 00:09:23 +0200

    Added summary for Feedback

--- SmoothieShop/Controllers/FeedbackController.cs
+++ SmoothieShop/Controllers/FeedbackController.cs
@@ -4,6 +4,9 @@ namespace SmoothieShop.Controllers
 {
     public class FeedbackController : Controller
     {
+        /// <summary>
+        /// Controls Feedback functionalities.
+        /// </summary>
         public IActionResult Index()
         {
             return View();
