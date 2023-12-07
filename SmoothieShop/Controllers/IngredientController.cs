2dcbf70260c86e9a0eaa03673906fd273fe94910
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-08 00:09:52 +0200

    Added summary for Ingredient

--- SmoothieShop/Controllers/IngredientController.cs
+++ SmoothieShop/Controllers/IngredientController.cs
@@ -4,6 +4,9 @@ namespace SmoothieShop.Controllers
 {
     public class IngredientController : Controller
     {
+        /// <summary>
+        /// Controls Ingredient functionalities.
+        /// </summary>
         public IActionResult Index()
         {
             return View();
