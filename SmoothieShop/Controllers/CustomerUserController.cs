74d3cf165471b304049eec8118e59750394a9e07
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-08 00:09:13 +0200

    Added summary for CustomerUser

--- SmoothieShop/Controllers/CustomerUserController.cs
+++ SmoothieShop/Controllers/CustomerUserController.cs
@@ -2,6 +2,9 @@
 
 namespace SmoothieShop.Controllers
 {
+    /// <summary>
+    /// Controls CustomerUser functionalities.
+    /// </summary>
     public class CustomerUserController : Controller
     {
         public IActionResult Index()
