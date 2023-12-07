a1513b2fc1d8ea313ecdfc1df6018a0fa4bcc208
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-07 23:29:31 +0200

    Added summary for GetCurrentUser

--- SmoothieShop/Common/GetCurrentUser.cs
+++ SmoothieShop/Common/GetCurrentUser.cs
@@ -2,13 +2,23 @@
 
 namespace SmoothieShop.Common
 {
+    /// <summary>
+    /// This class returns information about current(login) user.
+    /// </summary>
     public static class GetCurrentUser
     {
+        /// <summary>
+        /// This method returns current user Id.
+        /// </summary>
+        /// <param name="claimsPrincipalUser"></param>
         public static string GetCurrentUserId(this ClaimsPrincipal claimsPrincipalUser)
         {
             return claimsPrincipalUser.FindFirstValue(ClaimTypes.NameIdentifier);
         }
-        
+        /// <summary>
+        /// This method returns current user Name.
+        /// </summary>
+        /// <param name="claimsPrincipalUser"></param>
         public static string GetCurrentUserName(this ClaimsPrincipal claimsPrincipalUser)
         {
             return claimsPrincipalUser.FindFirstValue(ClaimTypes.Name);
