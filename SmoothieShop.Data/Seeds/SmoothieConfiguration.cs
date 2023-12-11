ee927c869f4f913b6a4c4394c5536e1f829d110f
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-11 13:50:07 +0200

    Updated SmoothieConfiguration with new data

--- SmoothieShop.Data/Seeds/SmoothieConfiguration.cs
+++ SmoothieShop.Data/Seeds/SmoothieConfiguration.cs
@@ -3,9 +3,12 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using SmoothieShop.Data.Data.Entites;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace SmoothieShop.Data.Seeds
 {
@@ -16,7 +19,38 @@ namespace SmoothieShop.Data.Seeds
     {
         public void Configure(EntityTypeBuilder<Smoothie> builder)
         {
-            throw new NotImplementedException();
+            builder.HasData(CreateSmoothies());
+        }
+        private static List<Smoothie> CreateSmoothies()
+        {
+            var smoothies = new List<Smoothie>()
+            {
+                new Smoothie()
+                {
+                   SmoothieId = 1,
+                   Name = "Banana Smoothie",
+                   // Ingredients - to finished lately
+                   Size = 250,
+                   Price = 4.50M,
+                   // Calories -  to finished lately
+                   MenuId = 1,
+                   OrderId = 1,
+                },
+
+                 new Smoothie()
+                {
+                   SmoothieId = 2,
+                   Name = "Strawberry Smoothie",
+                   // Ingredients - to finished lately
+                   Size = 250,
+                   Price = 5.50M,
+                   // Calories -  to finished lately
+                   MenuId = 1,
+                   OrderId = 2,
+                },
+            };
+
+            return smoothies;
         }
     }
 }
