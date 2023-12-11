5c940545ecf25adb1c3f0edd63be1fb997dbb518
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-11 13:31:33 +0200

    Updated IngredientConfiguration with new data

--- SmoothieShop.Data/Seeds/IngredientConfiguration.cs
+++ SmoothieShop.Data/Seeds/IngredientConfiguration.cs
@@ -16,7 +16,39 @@ namespace SmoothieShop.Data.Seeds
     {
         public void Configure(EntityTypeBuilder<Ingredient> builder)
         {
-            throw new NotImplementedException();
+            builder.HasData(CreateIngredients());
+        }
+
+        private static List<Ingredient> CreateIngredients()
+        {
+            var ingredients = new List<Ingredient>()
+            {
+                new Ingredient()
+                {
+                   IngredientId = 1,
+                   Name = "Banana",
+                   Calories = 105,
+                   IngredientInfo = "Bananas are berries, not fruits."
+                },
+
+                new Ingredient()
+                {
+                  IngredientId = 2,
+                  Name = "Kiwi",
+                  Calories = 42,
+                  IngredientInfo = "Kiwis are a type of berry and are related to strawberries and raspberries."
+                },
+
+                new Ingredient()
+                {
+                  IngredientId = 3,
+                  Name = "Strawberry",
+                  Calories = 32,
+                  IngredientInfo = "Strawberries are not actually berries, but rather an accessory fruit."
+                },
+            };
+
+            return ingredients;
         }
     }
 }
