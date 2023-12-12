3eadb2876bed2d133de3da0b138bcc872146cb6f
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-12 23:05:37 +0200

    Updated OrderConfiguration with new data

--- SmoothieShop.Data/Seeds/OrderConfiguration.cs
+++ SmoothieShop.Data/Seeds/OrderConfiguration.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using SmoothieShop.Data.Data.Entites;
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,41 +27,17 @@ namespace SmoothieShop.Data.Seeds
                 new Order()
                 {
                    OrderId = 1,
-                   Smoothies = new List<Smoothie>() { 
-                                                     new Smoothie { SmoothieId = 1,
-                                                                    Name = "Banana Smoothie",
-                                                                    Ingredients = new List<Ingredient>(){
-                                                                                               new Ingredient  { IngredientId = 1,
-                                                                                                                 Name = "Banana",
-                                                                                                                 Calories = 105,
-                                                                                                                 IngredientInfo = "Bananas are berries, not fruits."} },
-                                                                    Size = 250,
-                                                                    Price = 4.50M,
-                                                                    Calories = 210, //to make a method to calculate automatically
-                                                                    MenuId = 1,
-                                                                    OrderId = 1,} },
+                   // Smoothies - to finished lately
                    Price = 4.50M,
-                   Date = DateTime.ParseExact("12/12/2023", "dd/MM/yyyy", CultureInfo.InvariantCulture),
+                   // Date - to finish lately
                    CustomerId = 1
                 },
-                new Order()
+                 new Order()
                 {
                    OrderId = 2,
-                   Smoothies = new List<Smoothie>() {
-                                                      new Smoothie { SmoothieId = 2,
-                                                                     Name = "Strawberry Smoothie",
-                                                                     Ingredients = new List<Ingredient>(){
-                                                                                                new Ingredient  { IngredientId = 3,
-                                                                                                                  Name = "Strawberry",
-                                                                                                                  Calories = 32,
-                                                                                                                  IngredientInfo = "Strawberries are not actually berries, but rather an accessory fruit."} },                                        
-                                                                     Size = 250,
-                                                                     Price = 5.50M,
-                                                                     Calories =  480, //to make a method to calculate automatically                                 
-                                                                     MenuId = 1,
-                                                                     OrderId = 2,} },
+                   // Smoothies - to finished lately
                    Price = 5.50M,
-                   Date = DateTime.ParseExact("12/12/2023", "dd/MM/yyyy", CultureInfo.InvariantCulture),
+                   // Date - to finish lately
                    CustomerId = 2
                 },
 
