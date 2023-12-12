0ecf90badc2c897e6c9e020c698243523989ea12
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-12 23:10:53 +0200

    Updated OrderConfiguration with new data

--- SmoothieShop.Data/Seeds/OrderConfiguration.cs
+++ SmoothieShop.Data/Seeds/OrderConfiguration.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
 using SmoothieShop.Data.Data.Entites;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,17 +28,41 @@ namespace SmoothieShop.Data.Seeds
                 new Order()
                 {
                    OrderId = 1,
-                   // Smoothies - to finished lately
+                   Smoothies = new List<Smoothie>() {
+                                                     new Smoothie { SmoothieId = 1,
+                                                                    Name = "Banana Smoothie",
+                                                                    Ingredients = new List<Ingredient>(){
+                                                                                               new Ingredient  { IngredientId = 1,
+                                                                                                                 Name = "Banana",
+                                                                                                                 Calories = 105,
+                                                                                                                 IngredientInfo = "Bananas are berries, not fruits."} },
+                                                                    Size = 250,
+                                                                    Price = 4.50M,
+                                                                    Calories = 210, //to make a method to calculate automatically
+                                                                    MenuId = 1,
+                                                                    OrderId = 1,} },
                    Price = 4.50M,
-                   // Date - to finish lately
+                   Date = DateTime.ParseExact("12/12/2023", "dd/MM/yyyy", CultureInfo.InvariantCulture),
                    CustomerId = 1
                 },
-                 new Order()
+                new Order()
                 {
                    OrderId = 2,
-                   // Smoothies - to finished lately
+                   Smoothies = new List<Smoothie>() {
+                                                      new Smoothie { SmoothieId = 2,
+                                                                     Name = "Strawberry Smoothie",
+                                                                     Ingredients = new List<Ingredient>(){
+                                                                                                new Ingredient  { IngredientId = 3,
+                                                                                                                  Name = "Strawberry",
+                                                                                                                  Calories = 32,
+                                                                                                                  IngredientInfo = "Strawberries are not actually berries, but rather an accessory fruit."} },
+                                                                     Size = 250,
+                                                                     Price = 5.50M,
+                                                                     Calories =  480, //to make a method to calculate automatically                                 
+                                                                     MenuId = 1,
+                                                                     OrderId = 2,} },
                    Price = 5.50M,
-                   // Date - to finish lately
+                   Date = DateTime.ParseExact("12/12/2023", "dd/MM/yyyy", CultureInfo.InvariantCulture),
                    CustomerId = 2
                 },
 
