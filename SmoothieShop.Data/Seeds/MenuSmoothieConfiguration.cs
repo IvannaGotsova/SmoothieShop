9bccdb8512230d71af2c17b890b4bac9e1dcddd3
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-14 18:24:52 +0200

    Created MenuSmoothieConfiguration

---
+++ SmoothieShop.Data/Seeds/MenuSmoothieConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SmoothieShop.Data.Data.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmoothieShop.Data.Seeds
+{
+    /// <summary>
+    /// Holds MenuSmoothie Configuration.
+    /// </summary>
+    internal class MenuSmoothieConfiguration : IEntityTypeConfiguration<MenuSmoothie>
+    {
+        public void Configure(EntityTypeBuilder<MenuSmoothie> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
