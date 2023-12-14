0e2140822ee28b272b949c1a549b063fc7097b28
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-14 21:11:51 +0200

    Added IngredientSmoothieConfiguration and OrderSmoothieConfiguration

---
+++ SmoothieShop.Data/Seeds/IngredientSmoothieConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SmoothieShop.Data.Data.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmoothieShop.Data.Seeds
+{
+    /// <summary>
+    /// Holds IngredientSmoothie Configuration.
+    /// </summary>
+    internal class IngredientSmoothieConfiguration : IEntityTypeConfiguration<IngredientSmoothie>
+    {
+        public void Configure(EntityTypeBuilder<IngredientSmoothie> builder)
+        {
+            builder.HasData(CreateIngredientSmoothies());
+        }
+
+        // A method used to seed DB with initial data of IngredientSmoothie.
+        private static List<IngredientSmoothie> CreateIngredientSmoothies()
+        {
+            var ingredientSmoothies = new List<IngredientSmoothie>()
+            {
+                new IngredientSmoothie()
+                {
+                   IngredientId = 1,
+                   SmoothieId = 1
+                },
+
+                 new IngredientSmoothie()
+                {
+                   IngredientId = 3,
+                   SmoothieId = 2
+                },
+            };
+
+            return ingredientSmoothies;
+        }
+    }
+}
---
+++ SmoothieShop.Data/Seeds/OrderSmoothieConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SmoothieShop.Data.Data.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmoothieShop.Data.Seeds
+{
+    /// <summary>
+    /// Holds OrderSmoothie Configuration.
+    /// </summary>
+    internal class OrderSmoothieConfiguration : IEntityTypeConfiguration<OrderSmoothie>
+    {
+        public void Configure(EntityTypeBuilder<OrderSmoothie> builder)
+        {
+            builder.HasData(CreateOrderSmoothies());
+        }
+
+        // A method used to seed DB with initial data of OrderSmoothie.
+        private static List<OrderSmoothie> CreateOrderSmoothies()
+        {
+            var orderSmoothies = new List<OrderSmoothie>()
+            {
+                new OrderSmoothie()
+                {
+                   OrderId = 1,
+                   SmoothieId = 1
+                },
+
+                 new OrderSmoothie()
+                {
+                   OrderId = 2,
+                   SmoothieId = 2
+                },
+            };
+
+            return orderSmoothies;
+        }
+    }
+}
