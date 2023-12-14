af7d151d6bff57534c7571bbf17739542f0a3aa6
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-14 18:25:31 +0200

    Updated OrderConfiguration with new corrections

--- SmoothieShop.Data/Seeds/OrderConfiguration.cs
+++ SmoothieShop.Data/Seeds/OrderConfiguration.cs
@@ -27,40 +27,14 @@ namespace SmoothieShop.Data.Seeds
             {
                 new Order()
                 {
-                   OrderId = 1,
-                   Smoothies = new List<Smoothie>() {
-                                                     new Smoothie { SmoothieId = 1,
-                                                                    Name = "Banana Smoothie",
-                                                                    Ingredients = new List<Ingredient>(){
-                                                                                               new Ingredient  { IngredientId = 1,
-                                                                                                                 Name = "Banana",
-                                                                                                                 Calories = 105,
-                                                                                                                 IngredientInfo = "Bananas are berries, not fruits."} },
-                                                                    Size = 250,
-                                                                    Price = 4.50M,
-                                                                    Calories = 210, //to make a method to calculate automatically
-                                                                    MenuId = 1,
-                                                                    OrderId = 1,} },
+                   OrderId = 1,               
                    Price = 4.50M,
                    Date = DateTime.ParseExact("12/12/2023", "dd/MM/yyyy", CultureInfo.InvariantCulture),
                    CustomerId = 1
                 },
                 new Order()
                 {
-                   OrderId = 2,
-                   Smoothies = new List<Smoothie>() {
-                                                      new Smoothie { SmoothieId = 2,
-                                                                     Name = "Strawberry Smoothie",
-                                                                     Ingredients = new List<Ingredient>(){
-                                                                                                new Ingredient  { IngredientId = 3,
-                                                                                                                  Name = "Strawberry",
-                                                                                                                  Calories = 32,
-                                                                                                                  IngredientInfo = "Strawberries are not actually berries, but rather an accessory fruit."} },
-                                                                     Size = 250,
-                                                                     Price = 5.50M,
-                                                                     Calories =  480, //to make a method to calculate automatically                                 
-                                                                     MenuId = 1,
-                                                                     OrderId = 2,} },
+                   OrderId = 2,               
                    Price = 5.50M,
                    Date = DateTime.ParseExact("12/12/2023", "dd/MM/yyyy", CultureInfo.InvariantCulture),
                    CustomerId = 2
