9812fb7a6212f58365e5e813b494a27c8e8c8dde
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-14 17:59:44 +0200

    Created mapping class MenuOrder

---
+++ SmoothieShop.Data/Data/Entites/MenuOrder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmoothieShop.Data.Data.Entites
+{
+    /// <summary>
+    /// This is mapping class od Menu and Order.
+    /// </summary>
+    public class MenuOrder
+    {
+        [Required]
+        public int MenuId { get; set; } 
+        [ForeignKey(nameof(MenuId))]
+        public Menu? Menu { get; set; }
+        [Required]
+        public int OrderId { get; set; }
+        [ForeignKey(nameof(OrderId))]
+        public Order? Order { get; set; }
+    }
+}
