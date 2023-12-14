08dc6fefa54fe3adb62d39734ad02d097ecc5c2b
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-14 18:00:17 +0200

    Sync

--- SmoothieShop.Data/Seeds/CustomerConfiguration.cs
+++ SmoothieShop.Data/Seeds/CustomerConfiguration.cs
@@ -34,26 +34,7 @@ namespace SmoothieShop.Data.Seeds
                    LastName = "Petrov",
                    Email = "guest1@guest.com",
                    PhoneNumber = "0000000000",
-                   Address = "Bulgaria",
-                   Orders = new List<Order>(){ 
-                                               new Order {
-                                                           OrderId = 1,
-                                                           Smoothies = new List<Smoothie>() {
-                                                                   new Smoothie { SmoothieId = 1,
-                                                                                  Name = "Banana Smoothie",
-                                                                                  Ingredients = new List<Ingredient>(){
-                                                                                                             new Ingredient  { IngredientId = 1,                             
-                                                                                                                               Name = "Banana",                       
-                                                                                                                               Calories = 105,                             
-                                                                                                                               IngredientInfo = "Bananas are berries, not fruits."}},
-                                                                                  Size = 250,
-                                                                                  Price = 4.50M,
-                                                                                  Calories = 210, //to make a method to calculate automatically                   
-                                                                                  MenuId = 1,
-                                                                                  OrderId = 1,} },
-                                                            Price = 4.50M,
-                                                            Date = DateTime.ParseExact("12/12/2023", "dd/MM/yyyy", CultureInfo.InvariantCulture),
-                                                            CustomerId = 1} }
+                   Address = "Bulgaria",                
                 },
 
                 new Customer()
@@ -63,26 +44,7 @@ namespace SmoothieShop.Data.Seeds
                    LastName = "Dimitrov",
                    Email = "guest2@guest.com",
                    PhoneNumber = "0000000000",
-                   Address = "Bulgaria",
-                   Orders = new List<Order>(){
-                                               new Order {
-                                                           OrderId = 2,
-                                                           Smoothies = new List<Smoothie>() {
-                                                                   new Smoothie { SmoothieId = 2,
-                                                                                  Name = "Strawberry Smoothie",
-                                                                                  Ingredients = new List<Ingredient>(){
-                                                                                                             new Ingredient  { IngredientId = 3,
-                                                                                                                               Name = "Strawberry",
-                                                                                                                               Calories = 32,
-                                                                                                                               IngredientInfo = "Strawberries are not actually berries, but rather an accessory fruit."}},
-                                                                                  Size = 250,
-                                                                                  Price = 5.50M,
-                                                                                  Calories =  480, //to make a method to calculate  automatic                        
-                                                                                  MenuId = 1,
-                                                                                  OrderId = 2,} },
-                                                           Price = 5.50M,
-                                                           Date = DateTime.ParseExact("12/12/2023", "dd/MM/yyyy", CultureInfo.InvariantCulture),
-                                                           CustomerId = 2} }
+                   Address = "Bulgaria",                
                 },
             };
 
--- SmoothieShop.Data/obj/Debug/net6.0/SmoothieShop.Data.AssemblyInfo.cs
+++ SmoothieShop.Data/obj/Debug/net6.0/SmoothieShop.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmoothieShop.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1674ebdd1fe76fabbd549cfcb9c49e4a90b768fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e79983ed5f3f51bd8770c3a77fe8017de50a0981")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmoothieShop.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmoothieShop.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
