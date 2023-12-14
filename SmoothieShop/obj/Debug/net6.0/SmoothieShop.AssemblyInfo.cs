bbcefd524ee572cd8ebfb6924b9cbc6717c84cac
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-15 00:22:59 +0200

    Sync

--- SmoothieShop/obj/Debug/net6.0/SmoothieShop.AssemblyInfo.cs
+++ SmoothieShop/obj/Debug/net6.0/SmoothieShop.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmoothieShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d8138b198fc46e2ffd2fa5fdbeeae213e4e1c57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+038a8d19cede38423f57d376d75ef85c663b76ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmoothieShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmoothieShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
