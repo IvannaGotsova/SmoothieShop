818759372bc67171ab04402642e9db68d51e70eb
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-14 21:14:30 +0200

    Added IngredientSmoothie and OrderSmoothie mapping classes

---
+++ SmoothieShop.Data/Data/Entites/IngredientSmoothie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmoothieShop.Data.Data.Entites
+{
+    /// <summary>
+    /// This is mapping class of Ingredient and Smoothie.
+    /// </summary>
+    public class IngredientSmoothie
+    {
+        [Required]
+        public int IngredientId { get; set; }
+        [ForeignKey(nameof(IngredientId))]
+        public Ingredient? Ingredient { get; set; }
+        [Required]
+        public int SmoothieId { get; set; }
+        [ForeignKey(nameof(SmoothieId))]
+        public Smoothie? Smoothie { get; set; }
+    }
+}
---
+++ SmoothieShop.Data/Data/Entites/OrderSmoothie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmoothieShop.Data.Data.Entites
+{
+    /// <summary>
+    /// This is mapping class of Order and Smoothie
+    /// </summary>
+    public class OrderSmoothie
+    {
+        [Required]
+        public int OrderId { get; set; }
+        [ForeignKey(nameof(OrderId))]
+        public Order? Order { get; set; }
+        [Required]
+        public int SmoothieId { get; set; }
+        [ForeignKey(nameof(SmoothieId))]
+        public Smoothie? Smoothie { get; set; }
+    }
+}
+
