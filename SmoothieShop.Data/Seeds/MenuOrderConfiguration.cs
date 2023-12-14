2ea5dcc137e974d59896f124addbb001ce7e8afc
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-14 18:25:13 +0200

    Created MenuOrderConfiguration

---
+++ SmoothieShop.Data/Seeds/MenuOrderConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmoothieShop.Data.Data.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmoothieShop.Data.Seeds
+{
+    /// <summary>
+    /// Holds MenuOrder Configuration.
+    /// </summary>
+    internal class MenuOrderConfiguration : IEntityTypeConfiguration<MenuOrder>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<MenuOrder> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
