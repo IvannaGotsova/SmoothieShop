53ddb68d96a01b57a16259e698cce89c37308a6d
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-14 18:00:00 +0200

    Created mapping class MenuSmoothie

---
+++ SmoothieShop.Data/Data/Entites/MenuSmoothie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmoothieShop.Data.Data.Entites
+{
+    /// <summary>
+    /// This is mapping class od Menu and Smoothie.
+    /// </summary>
+    public class MenuSmoothie
+    {
+        [Required]
+        public int MenuId { get; set; }
+        [ForeignKey(nameof(MenuId))]
+        public Menu? Menu { get; set; }
+        [Required]
+        public int SmoothieId { get; set; }
+        [ForeignKey(nameof(SmoothieId))]
+        public Smoothie? Smoothie { get; set; }
+    }
+}
