bb044de7f9ed959128d5505c2e5f9aea7bfee913
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-17 00:06:04 +0200

    Updated IApplicationUserService with methods

--- SmoothieShop.Core/Contracts/IApplicationUserService.cs
+++ SmoothieShop.Core/Contracts/IApplicationUserService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SmoothieShop.Data.Data.Entites;
+using SmoothieShop.Data.Models.ApplicationUserModels;
 
 namespace SmoothieShop.Core.Contracts
 {
@@ -11,5 +13,45 @@ namespace SmoothieShop.Core.Contracts
     /// </summary>
     public interface IApplicationUserService
     {
+        /// <summary>
+        /// This method returns IEnumerable of all users.
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<ApplicationUserModel>> GetApplicationUsers();
+        /// <summary>
+        /// This method returns IEnumerable of all VIP users.
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<ApplicationUserModel>> GetApplicationVIPUsers();
+        /// <summary>
+        /// This method returns particular user with given id.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        Task<ApplicationUser> GetApplicaionUserById(string userId);
+        /// <summary>
+        /// This method creates form for deleting a particular user with given id.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        Task<ApplicationUserModel> DeleteCreateForm(string userId);
+        /// <summary>
+        /// This method deletes a particular user with given id.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        Task Delete(string userId);
+        /// <summary>
+        /// This method gives a particular user with given id VIP Status.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        Task MakeVIP(string userId);
+        /// <summary>
+        /// This method removes VIP Status from a particular user with given id .
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        Task RemoveVIP(string userId);
     }
 }
