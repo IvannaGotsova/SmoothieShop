e9e40e7d3a4433e4e3a46c29467cdaedfb7d5ef9
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-17 00:06:33 +0200

    Updated ApplicationUserService with methods

--- SmoothieShop.Core/Services/ApplicationUserService.cs
+++ SmoothieShop.Core/Services/ApplicationUserService.cs
@@ -1,4 +1,8 @@
-﻿using SmoothieShop.Core.Contracts;
+﻿using Microsoft.EntityFrameworkCore;
+using SmoothieShop.Core.Contracts;
+using SmoothieShop.Data.Data.Entites;
+using SmoothieShop.Data.Models.ApplicationUserModels;
+using SmoothieShop.Data.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +16,160 @@ namespace SmoothieShop.Core.Services
     /// </summary>
     public class ApplicationUserService : IApplicationUserService
     {
+        private readonly IRepository data;
+        public ApplicationUserService(IRepository data)
+        {
+            this.data = data;
+        }
+        /// <summary>
+        /// This method deletes a particular user with given id.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public async Task Delete(string userId)
+        {
+
+            await this.data
+                .DeleteAsync<ApplicationUser>(userId);
+            await this.data
+                .SaveChangesAsync();
+        }
+        /// <summary>
+        /// This method creates form for deleting a particular user with given id.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public async Task<ApplicationUserModel> DeleteCreateForm(string userId)
+        {
+            var userToBeDeleted = await
+                GetApplicaionUserById(userId);
+
+            var deleteApplicationUserModel = new ApplicationUserModel()
+            {
+                Id = userToBeDeleted!.Id,
+                UserName = userToBeDeleted!.UserName,
+                FirstName = userToBeDeleted!.FirstName,
+                LastName = userToBeDeleted!.LastName,
+            };
+
+            return deleteApplicationUserModel;
+        }
+        /// <summary>
+        /// This method returns particular user with given id.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public async Task<ApplicationUser> GetApplicaionUserById(string userId)
+        {
+            //check if user is null
+            if (await this.data
+                .GetByIdAsync<ApplicationUser>(userId) == null)
+            {
+                throw new ArgumentNullException(null, nameof(userId));
+            }
+
+            return await
+               this.data
+               .AllReadonly<ApplicationUser>()
+               .Where(au => au.Id == userId)
+               .FirstAsync();
+        }
+        /// <summary>
+        /// This method returns IEnumerable of all users.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IEnumerable<ApplicationUserModel>> GetApplicationUsers()
+        {
+            var allUsers = await
+                this.data
+                .AllReadonly<ApplicationUser>()
+                .ToListAsync();
+
+            return allUsers
+                .Select(u => new ApplicationUserModel()
+                {
+                    Id = u.Id,
+                    UserName = u.UserName,
+                    FirstName = u.FirstName,
+                    LastName = u.LastName,
+                    //IsVIP = u.IsVIP is true ? "VIP" : "Regular"
+
+                })
+                .ToList();
+
+        }
+        /// <summary>
+        /// This method returns IEnumerable of all VIP users.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IEnumerable<ApplicationUserModel>> GetApplicationVIPUsers()
+        {
+            var vipUsers = await
+               this.data
+               .AllReadonly<ApplicationUser>()
+               //.Where(au => au.IsVIP == true)
+               .ToListAsync();
+
+            return vipUsers
+                .Select(u => new ApplicationUserModel()
+                {
+                    Id = u.Id,
+                    UserName = u.UserName,
+                    FirstName = u.FirstName,
+                    LastName = u.LastName,
+                    //IsVIP = u.IsVIP is true ? "VIP" : "Regular"
+
+                })
+                .ToList();
+        }
+        /// <summary>
+        /// This method gives a particular user with given id VIP Status.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public async Task MakeVIP(string userId)
+        {
+
+            var user = await
+                this.data
+                .AllReadonly<ApplicationUser>()
+                .Where(au => au.Id == userId)
+                .FirstOrDefaultAsync();
+            //check if user is null
+            if (user == null)
+            {
+                throw new ArgumentNullException(null, nameof(user));
+            }
+
+           //user.IsVIP = true;
+
+            this.data.Update<ApplicationUser>(user);
+            await this.data.SaveChangesAsync();
+        }
+        /// <summary>
+        /// This method removes VIP Status from a particular user with given id .
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public async Task RemoveVIP(string userId)
+        {
+            var user = await
+                this.data
+                .AllReadonly<ApplicationUser>()
+                .Where(au => au.Id == userId)
+                .FirstOrDefaultAsync();
+            //check if user is null
+            if (user == null)
+            {
+                throw new ArgumentNullException(null, nameof(user));
+            }
+
+            //user.IsVIP = false;
+
+            this.data.Update<ApplicationUser>(user);
+            await this.data.SaveChangesAsync();
+        }
+
     }
 }
+
