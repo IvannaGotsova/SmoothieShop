f3aac189941ad667074143516a4b953752ac74d2
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-23 14:18:36 +0200

    Added DeleteCustomerModel

---
+++ SmoothieShop.Data/Models/CustomerModels/DeleteCustomerModel.cs
@@ -0,0 +1,37 @@
+﻿using SmoothieShop.Data.Data.Entites;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SmoothieShop.Data.Data.DataConstants.DataConstants.CustomerConstants;
+
+namespace SmoothieShop.Data.Models.CustomerModels
+{
+    public class DeleteCustomerModel
+    {
+        [Required]
+        public int CustomerId { get; set; }
+        [Required]
+        [StringLength(CustomerMaxLengthName, MinimumLength = CustomerMinLengthName)]
+        public string? FirstName { get; set; }
+        [Required]
+        [StringLength(CustomerMaxLengthName, MinimumLength = CustomerMinLengthName)]
+        public string? LastName { get; set; }
+        [Required]
+        [StringLength(CustomerMaxLengthEmail, MinimumLength = CustomerMinLengthEmail)]
+        public string? Email { get; set; }
+        [Required]
+        [StringLength(CustomerMaxLengthPhoneNumber, MinimumLength = CustomerMinLengthPhoneNumber)]
+        public string? PhoneNumber { get; set; }
+        [Required]
+        [StringLength(CustomerMaxLengthAddress, MinimumLength = CustomerMinLengthAddress)]
+        public string? Address { get; set; }
+        [Required]
+        public int CustomerUserId { get; set; }
+        [ForeignKey(nameof(CustomerUserId))]
+        public CustomerUser? CustomerUser { get; set; }
+    }
+}
