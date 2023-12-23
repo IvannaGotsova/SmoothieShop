126a08bb1f754d9675dddd407bd28edb30acac71
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-23 14:15:34 +0200

    Modified CustomerController with Add, Edit and Delete methods, method showing all customers and method getting customer by id.

--- SmoothieShop/Controllers/CustomerController.cs
+++ SmoothieShop/Controllers/CustomerController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SmoothieShop.Common;
+using SmoothieShop.Core.Contracts;
+using SmoothieShop.Data.Models.CustomerModels;
+using static SmoothieShop.ErrorConstants.ErrorConstants.GlobalErrorConstants;
 
 namespace SmoothieShop.Controllers
 {
@@ -7,9 +11,237 @@ namespace SmoothieShop.Controllers
     /// </summary>
     public class CustomerController : Controller
     {
+        private readonly ICustomerService customerService;
+        private readonly ICustomerUserService customerUserService;
+        public CustomerController(ICustomerService customerService, ICustomerUserService customerUserService)
+        {
+            this.customerUserService = customerUserService;
+        }
         public IActionResult Index()
         {
             return View();
         }
+        /// <summary>
+        /// This method returns all available customers.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IActionResult> AllCustomers()
+        {
+            try
+            {
+                var customers = await
+                    customerService
+                   .GetAllCustomers();
+
+                return View(customers);
+            }
+            catch (Exception)
+            {
+
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+        }
+        [HttpGet]
+        public async Task<IActionResult> AddCustomer()
+        {
+            var modelCustomer = new AddCustomerModel()
+            {
+                //CustomerUsers = await
+                //customerUserService.GetCustomerUsersForSelect(),
+            };
+
+            return View(modelCustomer);
+        }
+        /// <summary>
+        /// This method is used to add a customer.
+        /// </summary>
+        /// <param name="addCustomerModel"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> AddCustomer(AddCustomerModel addCustomerModel)
+        {
+            //check if the model state is valid
+            if (!ModelState.IsValid)
+            {
+                //addCustomerModel.CustomerUsers = await
+                //customerUserService..GetCustomerUsersForSelect(),
+
+                return View(addCustomerModel);
+            }
+
+            try
+            {
+                await customerService
+                    .Add(addCustomerModel);
+
+                TempData["message"] = $"You have successfully added a customer!";
+
+                return RedirectToAction("AllCustomers", "Customer");
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", somethingWrong);
+
+                //addCustomerModel.CustomerUsers = await
+                //customerUserService..GetCustomerUsersForSelect(),
+
+                return View(addCustomerModel);
+            }
+
+        }
+        /// <summary>
+        /// This method returns a details about particular customer with a given id.
+        /// </summary>
+        /// <param name="customerId"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> DetailsCustomer(int customerId)
+        {
+            //check if the comment is null
+            if (
+                await customerService
+                .GetCustomerDetailsById(customerId) == null)
+            {
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+
+            try
+            {
+                var customerModel = await
+                customerService
+                .GetCustomerDetailsById(customerId);
+
+                return View(customerModel);
+            }
+            catch (Exception)
+            {
+
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+
+        }
+        /// <summary>
+        /// This metod creates a form for editing a particular customer with a given id.
+        /// </summary>
+        /// <param name="customerId"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> EditCustomer(int customerId)
+        {
+            //check if the comment is null
+            if (await customerService
+                .GetCustomerDetailsById(customerId) == null)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                var editFormModel = await
+                       customerService
+                       .EditCreateForm(customerId);
+
+                return View(editFormModel);
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+
+
+
+        }
+        /// <summary>
+        /// This method is used to edit a particular comment with given id.
+        /// </summary>
+        /// <param name="customerId"></param>
+        /// <param name="editCustomerModel"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> EditCustomer(int customerId, EditCustomerModel editCustomerModel)
+        {
+            //check if the comment is null
+            if (await customerService
+                .GetCustomerDetailsById(customerId) == null)
+            {
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+
+            try
+            {
+                await customerService
+                    .Edit(customerId, editCustomerModel);
+
+                TempData["message"] = $"You have successfully edited a customer!";
+
+                return RedirectToAction("AllCustomers", "Customer");
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", somethingWrong);
+
+                return View(editCustomerModel);
+            }
+        }
+        /// <summary>
+        /// This metod creates a form for deleting a particular customer with a given id.
+        /// </summary>
+        /// <param name="customerId"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> DeleteCustomer(int customerId)
+        {
+            //check if the comment is null
+            if (await customerService
+                .GetCustomerDetailsById(customerId) == null)
+            {
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+
+            try
+            {
+                var editFormModel = await
+               customerService
+               .DeleteCustomerForm(customerId);
+
+                return View(editFormModel);
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+
+        }
+        /// <summary>
+        /// This method is used to delete a particular customer.
+        /// </summary>
+        /// <param name="deleteCustomerModel"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> DeleteCustomer(DeleteCustomerModel deleteCustomerModel)
+        {
+            //check if the comment is null
+            if (await customerService
+                .GetCustomerById(deleteCustomerModel.CustomerId) == null)
+            {
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+
+            try
+            {
+                await customerService
+                    .Delete(deleteCustomerModel.CustomerId);
+
+                TempData["message"] = $"You have successfully deleted a customer!";
+
+                return RedirectToAction("AllCustomers", "Customer");
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", somethingWrong);
+
+                return View(deleteCustomerModel);
+            }
+        }
+
     }
 }
