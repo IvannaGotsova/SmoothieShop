bf90d0c3cf9a0b5f3142b38dd5448a84ba55869b
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-23 14:18:53 +0200

    Added DetailsCustomerModel

---
+++ SmoothieShop.Data/Models/CustomerModels/DetailsCustomerModel.cs
@@ -0,0 +1,43 @@
+﻿using SmoothieShop.Data.Data.Entites;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SmoothieShop.Data.Data.DataConstants.DataConstants.CustomerConstants;
+
+namespace SmoothieShop.Data.Models.CustomerModels
+{
+    public class DetailsCustomerModel
+    {
+        [Required]
+        public int CustomerId { get; set; }
+        [Required]
+        [StringLength(CustomerMaxLengthName, MinimumLength = CustomerMinLengthName)]
+        public string? FirstName { get; set; }
+        [Required]
+        [StringLength(CustomerMaxLengthName, MinimumLength = CustomerMinLengthName)]
+        public string? LastName { get; set; }
+        [Required]
+        [StringLength(CustomerMaxLengthEmail, MinimumLength = CustomerMinLengthEmail)]
+        public string? Email { get; set; }
+        [Required]
+        [StringLength(CustomerMaxLengthPhoneNumber, MinimumLength = CustomerMinLengthPhoneNumber)]
+        public string? PhoneNumber { get; set; }
+        [Required]
+        [StringLength(CustomerMaxLengthAddress, MinimumLength = CustomerMinLengthAddress)]
+        public string? Address { get; set; }
+        [Required]
+        public int CustomerUserId { get; set; }
+        [ForeignKey(nameof(CustomerUserId))]
+        public CustomerUser? CustomerUser { get; set; }
+        [Required]
+        public IEnumerable<Order> Orders { get; set; } = new List<Order>();
+        [Required]
+        public IEnumerable<Feedback> Feedbacks { get; set; } = new List<Feedback>();
+        public int OrdersCount { get; set; }
+        public int FeedbacksCount { get; set; }
+    }
+}
