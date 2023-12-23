2a502111cf05979f9c379de334101af8e783f1db
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-23 14:16:04 +0200

    Added ErrorConstans with GlobalConstants in it.

---
+++ SmoothieShop/ErrorConstants/ErrorConstants.cs
@@ -0,0 +1,23 @@
+﻿namespace SmoothieShop.ErrorConstants
+{
+    /// <summary>
+    /// This class holds Error constants messages.
+    /// </summary>
+   public static class ErrorConstants
+
+   {
+        public static class GlobalErrorConstants
+        {
+            public const string somethingWrong = "Something went wrong, please try again!";
+        }
+
+        public static class UserErrorConstants
+        {
+            public const string invalidLogin = "Invalid Login";
+            public const string unAvailableUserName = "This UserName is unavailable. Please try another one";
+            public const string unAvailableEmail = "This Email is unavailable. Please try another one";
+
+        }
+    }
+
+}
