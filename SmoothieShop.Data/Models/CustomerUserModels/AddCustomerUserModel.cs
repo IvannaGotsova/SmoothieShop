6652b50d5c5f1b71675cc7ecd50f8630328bee9c
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-24 22:12:48 +0200

    Added model

---
+++ SmoothieShop.Data/Models/CustomerUserModels/AddCustomerUserModel.cs
@@ -0,0 +1,19 @@
+﻿using SmoothieShop.Data.Data.Entites;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmoothieShop.Data.Models.CustomerUserModels
+{
+    public class AddCustomerUserModel
+    {
+        [Required]
+        public string ApplicationUserId { get; set; } = null!;
+        [ForeignKey(nameof(ApplicationUserId))]
+        public ApplicationUser? ApplicationUser { get; set; }
+    }
+}
