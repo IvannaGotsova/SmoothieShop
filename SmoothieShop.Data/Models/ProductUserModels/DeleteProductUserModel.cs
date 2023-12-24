c6e6ba326f2021f25684743f55545e0c1b4915b2
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-24 22:13:24 +0200

    Added model

---
+++ SmoothieShop.Data/Models/ProductUserModels/DeleteProductUserModel.cs
@@ -0,0 +1,21 @@
+﻿using SmoothieShop.Data.Data.Entites;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmoothieShop.Data.Models.ProductUserModels
+{
+    public class DeleteProductUserModel
+    {
+        [Required]
+        public int ProductUserId { get; set; }
+        [Required]
+        public string ApplicationUserId { get; set; } = null!;
+        [ForeignKey(nameof(ApplicationUserId))]
+        public ApplicationUser? ApplicationUser { get; set; }
+    }
+}
