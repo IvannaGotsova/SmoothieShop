7f8c92481d10f8bfcd1cfd77b6623f076f46e01f
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-25 22:23:10 +0200

    Modified CustomerUserController with methods

--- SmoothieShop/Controllers/CustomerUserController.cs
+++ SmoothieShop/Controllers/CustomerUserController.cs
@@ -1,4 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SmoothieShop.Core.Contracts;
+using SmoothieShop.Core.Services;
+using SmoothieShop.Data.Data.Entites;
+using SmoothieShop.Data.Models.CustomerModels;
+using SmoothieShop.Data.Models.CustomerUserModels;
+using static SmoothieShop.ErrorConstants.ErrorConstants.GlobalErrorConstants;
 
 namespace SmoothieShop.Controllers
 {
@@ -7,9 +13,239 @@ namespace SmoothieShop.Controllers
     /// </summary>
     public class CustomerUserController : Controller
     {
+        private readonly ICustomerUserService customerUserService;
+        private readonly IApplicationUserService applicationUserService;
+        public CustomerUserController(ICustomerUserService customerUserService, IApplicationUserService applicationUserService)
+        {
+            this.customerUserService = customerUserService;
+            this.applicationUserService = applicationUserService;   
+        }
+        /// <summary>
+        /// This method returns index view.
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Index()
         {
             return View();
         }
+        /// <summary>
+        /// This method returns all available customers.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IActionResult> AllCustomerUsers()
+        {
+            try
+            {
+                var customerUsers = await
+                    customerUserService
+                   .GetAllCustomerUsers();
+
+                return View(customerUsers);
+            }
+            catch (Exception)
+            {
+
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+        }
+        [HttpGet]
+        public async Task<IActionResult> AddCustomerUser()
+        {
+            var modelCustomerUser = new AddCustomerUserModel()
+            {
+                ApplicationUsers = await
+                applicationUserService.GetApplicationUsersForSelect(),
+            };
+
+            return View(modelCustomerUser);
+        }
+        /// <summary>
+        /// This method is used to add a customerUser.
+        /// </summary>
+        /// <param name="addCustomerUserModel"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> AddCustomerUser(AddCustomerUserModel addCustomerUserModel)
+        {
+            //check if the model state is valid
+            if (!ModelState.IsValid)
+            {
+                addCustomerUserModel.ApplicationUsers = await
+                applicationUserService.GetApplicationUsersForSelect();
+
+                return View(addCustomerUserModel);
+            }
+
+            try
+            {
+                await customerUserService
+                    .Add(addCustomerUserModel);
+
+                TempData["message"] = $"You have successfully added a customer user!";
+
+                return RedirectToAction("AllCustomerUsers", "CustomerUser");
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", somethingWrong);
+
+                return View(addCustomerUserModel);
+            }
+
+        }
+        /// <summary>
+        /// This method returns a details about particular customerUser with a given id.
+        /// </summary>
+        /// <param name="customerUserId"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> DetailsCustomerUser(int customerUserId)
+        {
+            //check if the customerUser is null
+            if (
+                await customerUserService
+                .GetCustomerUserDetailsById(customerUserId) == null)
+            {
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+
+            try
+            {
+                var customerUserModel = await
+                customerUserService
+                .GetCustomerUserDetailsById(customerUserId);
+
+                return View(customerUserModel);
+            }
+            catch (Exception)
+            {
+
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+
+        }
+        /// <summary>
+        /// This metod creates a form for editing a particular customerUser with a given id.
+        /// </summary>
+        /// <param name="customerUserId"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> EditCustomerUser(int customerUserId)
+        {
+            //check if the customerUser is null
+            if (await customerUserService
+                .GetCustomerUserDetailsById(customerUserId) == null)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                var editFormModel = await
+                       customerUserService
+                       .EditCreateForm(customerUserId);
+
+                return View(editFormModel);
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+
+
+
+        }
+        /// <summary>
+        /// This method is used to edit a particular customerUser with given id.
+        /// </summary>
+        /// <param name="customerUserId"></param>
+        /// <param name="editCustomerUserModel"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> EditCustomerUser(int customerUserId, EditCustomerUserModel editCustomerUserModel)
+        {
+            //check if the customerUser is null
+            if (await customerUserService
+                .GetCustomerUserDetailsById(customerUserId) == null)
+            {
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+
+            try
+            {
+                await customerUserService
+                    .Edit(customerUserId, editCustomerUserModel);
+
+                TempData["message"] = $"You have successfully edited a customer user!";
+
+                return RedirectToAction("AllCustomerUsers", "CustomerUser");
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", somethingWrong);
+
+                return View(editCustomerUserModel);
+            }
+        }
+        /// <summary>
+        /// This metod creates a form for deleting a particular customerUser with a given id.
+        /// </summary>
+        /// <param name="customerUserId"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> DeleteCustomerUser(int customerUserId)
+        {
+            //check if the customerUser is null
+            if (await customerUserService
+                .GetCustomerUserDetailsById(customerUserId) == null)
+            {
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+
+            try
+            {
+                var editFormModel = await
+               customerUserService
+               .DeleteCustomerUserForm(customerUserId);
+
+                return View(editFormModel);
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+
+        }
+        /// <summary>
+        /// This method is used to delete a particular customerUser.
+        /// </summary>
+        /// <param name="deleteCustomerUserModel"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> DeleteCustomerUser(DeleteCustomerUserModel deleteCustomerUserModel)
+        {
+            //check if the customerUser is null
+            if (await customerUserService
+                .GetCustomerUserById(deleteCustomerUserModel.CustomerUserId) == null)
+            {
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+
+            try
+            {
+                await customerUserService
+                    .Delete(deleteCustomerUserModel.CustomerUserId);
+                
+                TempData["message"] = $"You have successfully deleted a customer user!";
+
+                return RedirectToAction("AllCustomerUsers", "CustomerUser");
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", somethingWrong);
+
+                return View(deleteCustomerUserModel);
+            }
+        }
+
     }
 }
