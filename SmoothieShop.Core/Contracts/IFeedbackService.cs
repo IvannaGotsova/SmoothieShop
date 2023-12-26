e010695ee4fd5701706e156bae6f63421ad59824
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-26 09:48:13 +0200

    Modified IFeedbackService with methods

--- SmoothieShop.Core/Contracts/IFeedbackService.cs
+++ SmoothieShop.Core/Contracts/IFeedbackService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SmoothieShop.Data.Data.Entites;
+using SmoothieShop.Data.Models.CustomerModels;
+using SmoothieShop.Data.Models.FeedbackModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,9 +10,62 @@ using System.Threading.Tasks;
 namespace SmoothieShop.Core.Contracts
 {
     /// <summary>
-    /// Holds Interface forFeedback functionality.
+    /// Holds Interface for Feedback functionality.
     /// </summary>
     public interface IFeedbackService
     {
+        /// <summary>
+        /// This method returns IEnumerable of all feedbacks.
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<AllFeedbacksModel>> GetAllFeedbacks();
+        /// <summary>
+        /// This method is used to add a feedback.
+        /// </summary>
+        /// <param name="addFeedbackModel"></param>
+        /// <returns></returns>
+        Task Add(AddFeedbackModel addFeedbackModel);
+        /// <summary>
+        /// This method returns IEnumerable of all feedbacks used for Select.
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<Feedback>> GetFeedbacksForSelect();
+        /// <summary>
+        /// This method returns Details of particular feedback with a given id.
+        /// </summary>
+        /// <param name="feedbackId"></param>
+        /// <returns></returns>
+        Task<DetailsFeedbackModel> GetFeedbackDetailsById(int feedbackId);
+        /// <summary>
+        /// This method creates form for editing a feedback.
+        /// </summary>
+        /// <param name="feedbackId"></param>
+        /// <returns></returns>
+        Task<EditFeedbackModel> EditCreateForm(int feedbackId);
+        /// <summary>
+        /// This method is used to edit a particular feedback with a given id.
+        /// </summary>
+        /// <param name="feedbackId"></param>
+        /// <param name="editFeedbackModel"></param>
+        /// <returns></returns>
+        Task Edit(int feedbackId, EditFeedbackModel editFeedbackModel);
+        /// <summary>
+        /// This method returns a particular feedback with a given id.
+        /// </summary>
+        /// <param name="feedbackId"></param>
+        /// <returns></returns>
+        Task<Feedback> GetFeedbackById(int feedbackId);
+        /// <summary>
+        /// This method creates form for deleting a feedback.
+        /// </summary>
+        /// <param name="feedbackId"></param>
+        /// <returns></returns>
+        Task<DeleteFeedbackModel> DeleteFeedbackForm(int feedbackId);
+        /// <summary>
+        /// This method deletes a particular feedback with a given id.
+        /// </summary>
+        /// <param name="feedbackId"></param>
+        /// <returns></returns>
+        Task Delete(int feedbackId);
     }
 }
