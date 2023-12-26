5eeaf102b8cc261f3a46b6699b9e5cfa4030b75e
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-26 09:52:19 +0200

    Revert "Revert "Added AddFeedbackModel""
    This reverts commit de01150ad0a50f9ba86a5141d9bb3e74e157e99c.

---
+++ SmoothieShop.Data/Models/FeedbackModels/AddFeedbackModel.cs
@@ -0,0 +1,29 @@
+﻿using SmoothieShop.Data.Data.Entites;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SmoothieShop.Data.Data.DataConstants.DataConstants.FeedbackConstants;
+
+namespace SmoothieShop.Data.Models.FeedbackModels
+{
+    /// <summary>
+    /// Holds AddFeedbackModel class.
+    /// </summary>
+    public class AddFeedbackModel
+    {
+        [Required]
+        [StringLength(FeedbackMaxLengthRating, MinimumLength = FeedbackMinLengthRating)]
+        public int Rating { get; set; }
+        [Required]
+        [StringLength(FeedbackMaxLengthComment, MinimumLength = FeedbackMinLengthComment)]
+        public string? Comment { get; set; }
+        [Required]
+        public int CustomerId { get; set; }
+        [ForeignKey(nameof(CustomerId))]
+        public Customer? Customer { get; set; }
+    }
+}
