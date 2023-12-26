c2ac7ddf18b6e0cc884f3f3b01c81041c48e5bde
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-26 09:49:20 +0200

    Added AddFeedbackModel

---
+++ SmoothieShop.Data/Models/FeedbackModels/DetailsFeedbackModel.cs
@@ -0,0 +1,31 @@
+﻿using SmoothieShop.Data.Data.Entites;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SmoothieShop.Data.Data.DataConstants.DataConstants.FeedbackConstants;
+
+namespace SmoothieShop.Data.Models.FeedbackModels
+{
+    /// <summary>
+    /// Holds DetaillsFeedbackModel class.
+    /// </summary>
+    public class DetailsFeedbackModel
+    {
+        [Required]
+        public int FeedbackId { get; set; }
+        [Required]
+        [StringLength(FeedbackMaxLengthRating, MinimumLength = FeedbackMinLengthRating)]
+        public int Rating { get; set; }
+        [Required]
+        [StringLength(FeedbackMaxLengthComment, MinimumLength = FeedbackMinLengthComment)]
+        public string? Comment { get; set; }
+        [Required]
+        public int CustomerId { get; set; }
+        [ForeignKey(nameof(CustomerId))]
+        public Customer? Customer { get; set; }
+    }
+}
