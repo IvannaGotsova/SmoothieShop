959c80eedceedad37ed381c35a591cefc15e3db6
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-27 12:45:43 +0200

    Added AddMenuModel

---
+++ SmoothieShop.Data/Models/MenuModels/AddMenuModel.cs
@@ -0,0 +1,30 @@
+﻿using SmoothieShop.Data.Data.Entites;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SmoothieShop.Data.Data.DataConstants.DataConstants.MenuConstants;
+
+namespace SmoothieShop.Data.Models.MenuModels
+{
+    /// <summary>
+    /// Holds AddMenuModel class.
+    /// </summary>
+    public class AddMenuModel
+    {
+        [Required]
+        [StringLength(MenuMaxLengthName, MinimumLength = MenutMinLengthName)]
+        public string? MenuName { get; set; }
+        [Required]
+        public int ProductUserId { get; set; }
+        [ForeignKey(nameof(ProductUserId))]
+        public ProductUser? ProductUser { get; set; }
+        [Required]
+        public IEnumerable<Smoothie> Smoothies { get; set; } = new List<Smoothie>();
+        [Required]
+        public IEnumerable<Order> Orders { get; set; } = new List<Order> { };
+    }
+}
