ac384d3cf2b19db97f946b63970dd930d5910dfc
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-27 10:52:49 +0200

    Added model for Ingredient Entity

---
+++ SmoothieShop.Data/Models/IngredientModels/DeleteIngredientModel.cs
@@ -0,0 +1,31 @@
+﻿using SmoothieShop.Data.Data.Entites;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SmoothieShop.Data.Data.DataConstants.DataConstants.IngredientConstants;
+
+namespace SmoothieShop.Data.Models.IngredientModels
+{
+    /// <summary>
+    /// Holds DeleteIngredientModel class.
+    /// </summary>
+    public class DeleteIngredientModel
+    {
+        [Required]
+        public int IngredientId { get; set; }
+        [Required]
+        [StringLength(IngredientMaxLengthName, MinimumLength = IngredientMinLengthName)]
+        public string? IngredientName { get; set; }
+        [Required]
+        [Range(typeof(double), "0.00", "10000.00", ConvertValueInInvariantCulture = true)]
+        public double Calories { get; set; }
+        [Required]
+        [StringLength(IngredientMaxLengthInfo, MinimumLength = IngredientMinLengthInfo)]
+        public string? IngredientInfo { get; set; }
+        [Required]
+        public IEnumerable<Smoothie> Smoothies { get; set; } = new List<Smoothie>();
+    }
+}
