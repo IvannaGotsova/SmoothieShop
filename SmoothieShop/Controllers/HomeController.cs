ae82598ff53024a641a7b3d592a5eb686cf3de96
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-28 21:28:55 +0200

    Updated HomeController with methods

--- SmoothieShop/Controllers/HomeController.cs
+++ SmoothieShop/Controllers/HomeController.cs
@@ -17,32 +17,50 @@ namespace SmoothieShop.Controllers
         {
             return View();
         }
-
+        /// <summary>
+        /// This method creates news page.
+        /// </summary>
+        /// <returns></returns>
         public IActionResult News()
         {
             return View();
         }
-
+        /// <summary>
+        /// This method creates privacy page.
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Privacy()
         {
             return View();
         }
-
+        /// <summary>
+        /// This method creates contacts page.
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Contacts()
         {
             return View();
         }
-
+        /// <summary>
+        /// This method creates FAQs page.
+        /// </summary>
+        /// <returns></returns>
         public IActionResult FAQs()
         {
             return View();
         }
-
+        /// <summary>
+        /// This method creates AboutUs page.
+        /// </summary>
+        /// <returns></returns>
         public IActionResult AboutUs()
         {
             return View();
         }
-
+        /// <summary>
+        /// This method creates error page.
+        /// </summary>
+        /// <returns></returns>
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
