4fc1c430e3e1dcc196ab075cb66fb4d25462010f
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-28 21:35:50 +0200

    Updated service with basic methods

--- SmoothieShop.Core/Contracts/IOrderService.cs
+++ SmoothieShop.Core/Contracts/IOrderService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SmoothieShop.Data.Data.Entites;
+using SmoothieShop.Data.Models.OrderModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,5 +13,58 @@ namespace SmoothieShop.Core.Contracts
     /// </summary>
     public interface IOrderService
     {
+        /// <summary>
+        /// This method returns IEnumerable of all orders.
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<AllOrdersModel>> GetAllOrders();
+        /// <summary>
+        /// This method is used to add a order.
+        /// </summary>
+        /// <param name="addOrderModel"></param>
+        /// <returns></returns>
+        Task Add(AddOrderModel addOrderModel);
+        /// <summary>
+        /// This method returns IEnumerable of all orders used for Select.
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<Order>> GetOrdersForSelect();
+        /// <summary>
+        /// This method returns Details of particular order with a given id.
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <returns></returns>
+        Task<DetailsOrderModel> GetOrderDetailsById(int orderId);
+        /// <summary>
+        /// This method creates form for editing a order.
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <returns></returns>
+        Task<EditOrderModel> EditCreateForm(int orderId);
+        /// <summary>
+        /// This method is used to edit a particular order with a given id.
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <param name="editOrderModel"></param>
+        /// <returns></returns>
+        Task Edit(int orderId, EditOrderModel editOrderModel);
+        /// <summary>
+        /// This method returns a particular order with a given id.
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <returns></returns>
+        Task<Order> GetOrderById(int orderId);
+        /// <summary>
+        /// This method creates form for deleting a order.
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <returns></returns>
+        Task<DeleteOrderModel> DeleteOrderForm(int orderId);
+        /// <summary>
+        /// This method deletes a particular order with a given id.
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <returns></returns>
+        Task Delete(int orderId);
     }
 }
