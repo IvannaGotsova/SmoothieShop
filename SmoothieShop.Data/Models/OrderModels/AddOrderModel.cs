ce5078b9bcbf605dc34711cf38630564a6083c90
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-28 21:29:32 +0200

    Added model for Order Entity

---
+++ SmoothieShop.Data/Models/OrderModels/AddOrderModel.cs
@@ -0,0 +1,31 @@
+﻿using SmoothieShop.Data.Data.Entites;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmoothieShop.Data.Models.OrderModels
+{
+    /// <summary>
+    /// Holds AddOrderModel class.
+    /// </summary>
+    public class AddOrderModel
+    {
+        [Required]
+        [Range(typeof(double), "0.00", "10000.00", ConvertValueInInvariantCulture = true)]
+        public decimal Price { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public int CustomerId { get; set; }
+        [ForeignKey(nameof(CustomerId))]
+        public Customer? Customer { get; set; }
+        [Required]
+        public IEnumerable<Smoothie> Smoothies { get; set; } = new List<Smoothie>();
+        [Required]
+        public IEnumerable<Menu> Menus { get; set; } = new List<Menu>();
+    }
+}
