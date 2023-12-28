9c9f23cb7faccf8c73bdc2f703c4b7239f92ea24
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-28 21:28:38 +0200

    Updated SmoothieController with basic methods

--- SmoothieShop/Controllers/SmoothieController.cs
+++ SmoothieShop/Controllers/SmoothieController.cs
@@ -1,15 +1,268 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SmoothieShop.Core.Contracts;
+using SmoothieShop.Data.Data.Entites;
+using SmoothieShop.Data.Models.SmoothieModels;
+using static SmoothieShop.ErrorConstants.ErrorConstants.GlobalErrorConstants;
 
 namespace SmoothieShop.Controllers
-{
+{ 
+    /// <summary>
+    /// Controls Smoothie functionalities.
+    /// </summary>
     public class SmoothieController : Controller
     {
+        private readonly ISmoothieService smoothieService;
+        private readonly IMenuService menuService;
+        private readonly IOrderService orderService;
+        private readonly IIngredientService ingredientService;
+        public SmoothieController(ISmoothieService smoothieService, IMenuService menuUserService, IOrderService orderService, IIngredientService ingredientService)
+        {
+            this.smoothieService = smoothieService;
+            this.menuService = menuUserService;
+            this.orderService = orderService;
+            this.ingredientService = ingredientService;
+        }
         /// <summary>
-        /// Controls Smoothie functionalities.
+        /// This method returns index view.
         /// </summary>
+        /// <returns></returns>
         public IActionResult Index()
         {
             return View();
         }
+        /// <summary>
+        /// This method returns all available smoothies.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IActionResult> AllSmoothies()
+        {
+            try
+            {
+                var smoothies = await
+                    smoothieService
+                   .GetAllSmoothies();
+
+                return View(smoothies);
+            }
+            catch (Exception)
+            {
+
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+        }
+        [HttpGet]
+        public async Task<IActionResult> AddSmoothie()
+        {
+            var modelSmoothie = new AddSmoothieModel()
+            {
+                Menus = await
+                menuService.GetMenusForSelect(),
+                Orders = await
+                orderService.GetOrdersForSelect(),
+                Ingredients = await
+                ingredientService.GetIngredientsForSelect(),
+            };
+
+            return View(modelSmoothie);
+        }
+        /// <summary>
+        /// This method is used to add a smoothie.
+        /// </summary>
+        /// <param name="addSmoothieModel"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> AddSmoothie(AddSmoothieModel addSmoothieModel)
+        {
+            //check if the model state is valid
+            if (!ModelState.IsValid)
+            {
+                addSmoothieModel.Menus = await
+                menuService.GetMenusForSelect();
+                addSmoothieModel.Orders = await
+                orderService.GetOrdersForSelect();
+                addSmoothieModel.Ingredients = await
+                ingredientService.GetIngredientsForSelect();
+
+                return View(addSmoothieModel);
+            }
+
+            try
+            {
+                await smoothieService
+                    .Add(addSmoothieModel);
+
+                TempData["message"] = $"You have successfully added a smoothie!";
+
+                return RedirectToAction("AllSmoothies", "Smoothie");
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", somethingWrong);
+
+                addSmoothieModel.Menus = await
+                menuService.GetMenusForSelect();
+                addSmoothieModel.Orders = await
+                orderService.GetOrdersForSelect();
+                addSmoothieModel.Ingredients = await
+                ingredientService.GetIngredientsForSelect();
+
+                return View(addSmoothieModel);
+            }
+
+        }
+        /// <summary>
+        /// This method returns a details about particular smoothie with a given id.
+        /// </summary>
+        /// <param name="smoothieId"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> DetailsSmoothie(int smoothieId)
+        {
+            //check if the smoothie is null
+            if (
+                await smoothieService
+                .GetSmoothieDetailsById(smoothieId) == null)
+            {
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+
+            try
+            {
+                var smoothieModel = await
+                smoothieService
+                .GetSmoothieDetailsById(smoothieId);
+
+                return View(smoothieModel);
+            }
+            catch (Exception)
+            {
+
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+
+        }
+        /// <summary>
+        /// This metod creates a form for editing a particular smoothie with a given id.
+        /// </summary>
+        /// <param name="smoothieId"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> EditSmoothie(int smoothieId)
+        {
+            //check if the smoothie is null
+            if (await smoothieService
+                .GetSmoothieDetailsById(smoothieId) == null)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                var editFormModel = await
+                       smoothieService
+                       .EditCreateForm(smoothieId);
+
+                return View(editFormModel);
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+
+
+
+        }
+        /// <summary>
+        /// This method is used to edit a particular smoothie with a given id.
+        /// </summary>
+        /// <param name="smoothieId"></param>
+        /// <param name="editSmoothieModel"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> EditSmoothie(int smoothieId, EditSmoothieModel editSmoothieModel)
+        {
+            //check if the smoothie is null
+            if (await smoothieService
+                .GetSmoothieDetailsById(smoothieId) == null)
+            {
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+
+            try
+            {
+                await smoothieService
+                    .Edit(smoothieId, editSmoothieModel);
+
+                TempData["message"] = $"You have successfully edited a smoothie!";
+
+                return RedirectToAction("AllSmoothies", "Smoothie");
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", somethingWrong);
+
+                return View(editSmoothieModel);
+            }
+        }
+        /// <summary>
+        /// This metod creates a form for deleting a particular smoothie with a given id.
+        /// </summary>
+        /// <param name="smoothieId"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> DeleteSmoothie(int smoothieId)
+        {
+            //check if the smoothie is null
+            if (await smoothieService
+                .GetSmoothieDetailsById(smoothieId) == null)
+            {
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+
+            try
+            {
+                var editFormModel = await
+               smoothieService
+               .DeleteSmoothieForm(smoothieId);
+
+                return View(editFormModel);
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+
+        }
+        /// <summary>
+        /// This method is used to delete a particular smoothie.
+        /// </summary>
+        /// <param name="deleteSmoothieModel"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> DeleteSmoothie(DeleteSmoothieModel deleteSmoothieModel)
+        {
+            //check if the smoothie is null
+            if (await smoothieService
+                .GetSmoothieById(deleteSmoothieModel.SmoothieId) == null)
+            {
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+
+            try
+            {
+                await smoothieService
+                    .Delete(deleteSmoothieModel.SmoothieId);
+
+                TempData["message"] = $"You have successfully deleted a smoothie!";
+
+                return RedirectToAction("AllSmoothies", "Smoothie");
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", somethingWrong);
+
+                return View(deleteSmoothieModel);
+            }
+        }
+
     }
 }
