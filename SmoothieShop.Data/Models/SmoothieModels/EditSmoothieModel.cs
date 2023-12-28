e0f5f000bc0ab07237052dcf086f2f304a1d421b
IvannaGotsova <ivannagotsova@outlook.com>
2023-12-28 21:31:03 +0200

    Added model for Smoothie Entity

---
+++ SmoothieShop.Data/Models/SmoothieModels/EditSmoothieModel.cs
@@ -0,0 +1,37 @@
+﻿using SmoothieShop.Data.Data.Entites;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SmoothieShop.Data.Data.DataConstants.DataConstants.SmoothieConstants;
+
+namespace SmoothieShop.Data.Models.SmoothieModels
+{
+    /// <summary>
+    /// Holds EditSmoothieModel class.
+    /// </summary>
+    public class EditSmoothieModel
+    {
+        [Required]
+        public int SmoothieId { get; set; }
+        [Required]
+        [StringLength(SmoothieMaxLengthName, MinimumLength = SmoothieMinLengthName)]
+        public string? SmoothieName { get; set; }
+        [Range(typeof(double), "0.00", "10000.00", ConvertValueInInvariantCulture = true)]
+        public double Size { get; set; }
+        [Required]
+        [Range(typeof(decimal), "0.00", "10000.00", ConvertValueInInvariantCulture = true)]
+        public decimal Price { get; set; }
+        [Required]
+        [Range(typeof(double), "0.00", "10000.00", ConvertValueInInvariantCulture = true)]
+        public double Calories { get; set; }
+        [Required]
+        public IEnumerable<Menu> Menus { get; set; } = new List<Menu> { };
+        [Required]
+        public IEnumerable<Order> Orders { get; set; } = new List<Order> { };
+        [Required]
+        public IEnumerable<Ingredient> Ingredients { get; set; } = new List<Ingredient> { };
+    }
+}
