d97be47adc279e0d3a10621fe7ee999200d4bc80
IvannaGotsova <ivannagotsova@outlook.com>
2024-01-08 21:35:05 +0200

    Modified app.UseEndpoints

--- SmoothieShop/Program.cs
+++ SmoothieShop/Program.cs
@@ -84,16 +84,15 @@ app.UseAuthorization();
 app.UseEndpoints(endpoints =>
 {
 
-    app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}"
-    );
-
-
 
     app.MapControllerRoute(
     name: "areas",
     pattern: "{area:exists}/{controller=Admin}/{action=Index}/{id?}"
+    );
+
+    app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
     );
 
 });
