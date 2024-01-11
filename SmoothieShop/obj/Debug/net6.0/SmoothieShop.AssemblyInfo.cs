9366ee4a4fdedbe4de2b13b6360f9795f32cb888
IvannaGotsova <ivannagotsova@outlook.com>
2024-01-11 22:27:20 +0200

    Sync - all basic methods in OrderController run right

--- SmoothieShop/obj/Debug/net6.0/SmoothieShop.AssemblyInfo.cs
+++ SmoothieShop/obj/Debug/net6.0/SmoothieShop.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmoothieShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02209d7f6c19f70729f20e63a297c73e8e3dbc38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc6d82ea64b452b892742c036788ba610370b2fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmoothieShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmoothieShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
