0a003e92bc32b81f29d7ba7cb78c1e4a680dea17
IvannaGotsova <ivannagotsova@outlook.com>
2024-01-12 22:27:53 +0200

    Sync - all basic methods in FeedbackController run right

--- SmoothieShop/obj/Debug/net6.0/SmoothieShop.AssemblyInfo.cs
+++ SmoothieShop/obj/Debug/net6.0/SmoothieShop.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmoothieShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc6d82ea64b452b892742c036788ba610370b2fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9366ee4a4fdedbe4de2b13b6360f9795f32cb888")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmoothieShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmoothieShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
