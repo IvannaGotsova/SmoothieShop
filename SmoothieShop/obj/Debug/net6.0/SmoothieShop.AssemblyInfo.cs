94d8c070fe5e375936f4f3cc05743e1bfcf7ac9e
IvannaGotsova <ivannagotsova@outlook.com>
2024-01-14 17:02:01 +0200

    Sync - all basic methods in CustomerController run right

--- SmoothieShop/obj/Debug/net6.0/SmoothieShop.AssemblyInfo.cs
+++ SmoothieShop/obj/Debug/net6.0/SmoothieShop.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmoothieShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a003e92bc32b81f29d7ba7cb78c1e4a680dea17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab0221977a95043c0e0741ade8e2dc69af7b0fd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmoothieShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmoothieShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
