d074802506ddf2962d417e96924680b8286255a2
IvannaGotsova <ivannagotsova@outlook.com>
2024-01-25 22:36:03 +0200

    Creating ErrorController

---
+++ SmoothieShop/Controllers/ErrorController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmoothieShop.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult PageNotFoundError()
+        {
+            return View();
+        }
+        public ActionResult UnauthorizedError()
+        {
+            return View();
+        }
+        public ActionResult InternalServerError()
+        {
+            return View();
+        }
+        public ActionResult GenericError()
+        {
+            return View();
+        }
+    }
+}
