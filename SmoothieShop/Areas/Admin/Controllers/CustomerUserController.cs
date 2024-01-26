b5f456fa8982cd70bb6391414264587091c1bd05
IvannaGotsova <ivannagotsova@outlook.com>
2024-01-26 23:30:57 +0200

    Adding authorization in Controller with area Admin

--- SmoothieShop/Areas/Admin/Controllers/CustomerUserController.cs
+++ SmoothieShop/Areas/Admin/Controllers/CustomerUserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using SmoothieShop.Core.Contracts;
 using SmoothieShop.Core.Services;
 using SmoothieShop.Data.Data.Entites;
@@ -8,10 +9,11 @@ using static SmoothieShop.ErrorConstants.ErrorConstants.GlobalErrorConstants;
 
 namespace SmoothieShop.Areas.Admin.Controllers
 {
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
     /// <summary>
     /// Controls CustomerUser functionalities of Admin.
     /// </summary>
-    [Area("Admin")]
     public class CustomerUserController : Controller
     {
         private readonly ICustomerUserService customerUserService;
