b2a365c0f25f27f8a7445ddea1a2b4ac097a16fd
IvannaGotsova <ivannagotsova@outlook.com>
2024-01-26 23:30:06 +0200

    Adding authorization in Controller with area Admin

--- SmoothieShop/Areas/Admin/Controllers/AdminController.cs
+++ SmoothieShop/Areas/Admin/Controllers/AdminController.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace SmoothieShop.Areas.Admin.Controllers
 {
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
     /// <summary>
     ///  Controls all functions of Admin;
     /// </summary>
-    [Area("Admin")]
     public class AdminController : Controller
     {
         public IActionResult Index()
