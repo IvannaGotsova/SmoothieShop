95837cc66427ef491a94205399f62f386c8d45dd
IvannaGotsova <ivannagotsova@outlook.com>
2025-11-02 18:02:12 +0200

    Created additional application user models

---
+++ SmoothieShop.Data/Models/ApplicationUserModels/AddApplicationUserModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SmoothieShop.Data.Data.DataConstants.DataConstants.ApplicationUserConstants;
+
+namespace SmoothieShop.Data.Models.ApplicationUserModels
+{
+    public class AddApplicationUserModel
+    {
+        [Required]
+        [StringLength(ApplicationUserMaxLengthName, MinimumLength = ApplicationUserMinLengthName)]
+        public string UserName { get; set; } = null!;
+        [Required]
+        [StringLength(ApplicationUserMaxLengthName, MinimumLength = ApplicationUserMinLengthName)]
+        public string? FirstName { get; set; }
+        [Required]
+        [StringLength(ApplicationUserMaxLengthName, MinimumLength = ApplicationUserMinLengthName)]
+        public string? LastName { get; set; }
+    }
+}
