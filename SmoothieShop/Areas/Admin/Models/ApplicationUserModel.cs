462ad4c2f916fedd774639f1e4d02c8151630469
IvannaGotsova <ivannagotsova@outlook.com>
2025-11-10 10:41:32 +0200

    Created new model

---
+++ SmoothieShop/Areas/Admin/Models/ApplicationUserModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SmoothieShop.Data.Data.DataConstants.DataConstants.ApplicationUserConstants;
+
+
+namespace SmoothieShop.Areas.Admin.Models
+{
+    /// <summary>
+    /// Holds ApplicationUserModel class.
+    /// </summary>
+    public class ApplicationUserModel
+    {
+        public string Id { get; set; } = null!;
+        [Required]
+        [StringLength(ApplicationUserMaxLengthName, MinimumLength = ApplicationUserMinLengthName)]
+        public string UserName { get; set; } = null!;
+        [Required]
+        [StringLength(ApplicationUserMaxLengthName, MinimumLength = ApplicationUserMinLengthName)]
+        public string? FirstName { get; set; }
+        [Required]
+        [StringLength(ApplicationUserMaxLengthName, MinimumLength = ApplicationUserMinLengthName)]
+        public string? LastName { get; set; }
+    }
+}
