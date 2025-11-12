580949d92d1def57259c383d9525a9f4bd97ab44
IvannaGotsova <ivannagotsova@outlook.com>
2025-11-12 11:41:35 +0200

    Added new functionalities to Admin Controller

--- SmoothieShop/Areas/Admin/Controllers/AdminController.cs
+++ SmoothieShop/Areas/Admin/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SmoothieShop.Core.Contracts;
 
 namespace SmoothieShop.Areas.Admin.Controllers
 {
@@ -10,9 +11,165 @@ namespace SmoothieShop.Areas.Admin.Controllers
     /// </summary>
     public class AdminController : Controller
     {
+        private readonly IApplicationUserService applicationUser;
+        private readonly ICustomerService customerService;
+        private readonly ICustomerUserService customerUserService;
+        private readonly IFeedbackService feedbackService;
+        private readonly IIngredientService ingredientService;
+        private readonly IMenuService menuService;
+        private readonly IOrderService orderService;
+        private readonly IProductUserService productUserService;
+        private readonly ISmoothieService smoothieService;
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult AllApplicationUsersCount()
+        {
+            try
+            {
+                var count =
+                applicationUser
+                .Count();
+
+                return View(count);
+            }
+            catch (Exception)
+            {
+
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+        }
+
+        public IActionResult AllCustomersCount()
+        {
+            try
+            {
+                var count =
+                customerService
+                .Count();
+
+                return View(count);
+            }
+            catch (Exception)
+            {
+
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+        }
+        public IActionResult AllCustomerUsersCount()
+        {
+            try
+            {
+                var count =
+                customerUserService
+                .Count();
+
+                return View(count);
+            }
+            catch (Exception)
+            {
+
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+        }
+        public IActionResult AllFeedbacksCount()
+        {
+            try
+            {
+                var count =
+                feedbackService
+                .Count();
+
+                return View(count);
+            }
+            catch (Exception)
+            {
+
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+        }
+        public IActionResult AllIngredientsCount()
+        {
+            try
+            {
+                var count =
+                ingredientService
+                .Count();
+
+                return View(count);
+            }
+            catch (Exception)
+            {
+
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+        }
+        public IActionResult AllMenusCount()
+        {
+            try
+            {
+                var count =
+                menuService
+                .Count();
+
+                return View(count);
+            }
+            catch (Exception)
+            {
+
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+        }
+        public IActionResult AllOrdersCount()
+        {
+            try
+            {
+                var count =
+                orderService
+                .Count();
+
+                return View(count);
+            }
+            catch (Exception)
+            {
+
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+        }
+        public IActionResult AllProductUsersCount()
+        {
+            try
+            {
+                var count =
+                productUserService
+                .Count();
+
+                return View(count);
+            }
+            catch (Exception)
+            {
+
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
+        }
+        public IActionResult AllSmoothiesCount()
+        {
+            try
+            {
+                var count =
+                smoothieService
+                .Count();
+
+                return View(count);
+            }
+            catch (Exception)
+            {
+
+                return RedirectToAction("Error", "Home", new { area = "" });
+            }
         }
     }
 }
--- SmoothieShop/obj/Debug/net6.0/SmoothieShop.AssemblyInfo.cs
+++ SmoothieShop/obj/Debug/net6.0/SmoothieShop.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmoothieShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+160dd8085ebac4b6ecc87bb834938d7c2887002f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+042b1cc667eb6425117ad9a1e0deaa1787037b6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmoothieShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmoothieShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
