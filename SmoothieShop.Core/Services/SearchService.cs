ad75979e58c0473129564a0aa9076c002c2d466e
IvannaGotsova <ivannagotsova@outlook.com>
2025-11-20 17:39:55 +0200

    Modified Service for Search functionality

--- SmoothieShop.Core/Services/SearchService.cs
+++ SmoothieShop.Core/Services/SearchService.cs
@@ -32,10 +32,56 @@ namespace SmoothieShop.Core.Services
                 .Where(m => m.MenuName.Contains(query))
                 .ToList();
 
+            var ingredients = data
+                 .AllReadonly<Ingredient>()
+                 .Where(i => i.IngredientName.Contains(query) || i.IngredientInfo.Contains(query))
+                 .ToList();
+
+            var feedbacks = data
+                 .AllReadonly<Feedback>()
+                 .Where(f => f.Comment.Contains(query) || f.Rating.ToString().Contains(query))
+                 .ToList();
+
+            var customers = data
+                .AllReadonly<Customer>()
+                .Where(c => c.FirstName.Contains(query) || c.LastName.Contains(query) || c.Address.Contains(query) || c.Email.Contains(query) || c.PhoneNumber.Contains(query))
+                .ToList();
+
+            var orders = data
+                 .AllReadonly<Order>()
+                 .Where(o => o.OrderId.ToString().Contains(query))
+                 .ToList();
+
+            var applicationUsers = data
+                .AllReadonly<ApplicationUser>()
+                .Where(au => au.UserName.Contains(query))
+                .ToList();
+
+            var customerUsers = data
+                .AllReadonly<CustomerUser>()
+                .Include( cu => cu.ApplicationUser)
+                .Where(cu => cu.ApplicationUser.UserName.Contains(query))
+                .ToList();
+
+            var productUsers = data
+                .AllReadonly<ProductUser>()
+                 .Include(pu => pu.ApplicationUser)
+                .Where(pu => pu.ApplicationUser.UserName.Contains(query))
+                .ToList();
+
+
             return new SearchResultModel
             {
                 Smoothies = smoothies,
-                Menus = menus
+                Menus = menus,
+                Ingredients = ingredients,
+                Feedbacks = feedbacks,
+                Customers = customers,
+                Orders = orders,
+                ApplicationUsers = applicationUsers,
+                CustomerUsers = customerUsers,
+                ProductUsers = productUsers
+
             };
         }
     }
