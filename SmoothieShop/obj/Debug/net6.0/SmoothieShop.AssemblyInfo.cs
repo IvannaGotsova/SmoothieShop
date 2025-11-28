90a05d18f6b61ca87fe9f1ec622b7457e7c1cc53
IvannaGotsova <ivannagotsova@outlook.com>
2025-11-28 20:57:03 +0200

    Change name of table title

--- SmoothieShop/obj/Debug/net6.0/SmoothieShop.AssemblyInfo.cs
+++ SmoothieShop/obj/Debug/net6.0/SmoothieShop.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmoothieShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc7137d64eadf81f315bb4e1f3c46b5949f15b10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8de41ed1d905bb5d4ba138d60fd54f360469576c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmoothieShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmoothieShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
