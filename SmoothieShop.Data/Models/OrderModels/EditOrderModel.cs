90d13c449b2f183b00a84e01c3a4f2c700606a77
IvannaGotsova <ivannagotsova@outlook.com>
2025-12-16 18:01:45 +0200

    Modified Model for Customer Select

--- SmoothieShop.Data/Models/OrderModels/EditOrderModel.cs
+++ SmoothieShop.Data/Models/OrderModels/EditOrderModel.cs
@@ -27,5 +27,26 @@ namespace SmoothieShop.Data.Models.OrderModels
         public Customer? Customer { get; set; }
         public IEnumerable<Smoothie> Smoothies { get; set; } = new List<Smoothie>();
         public IEnumerable<Menu> Menus { get; set; } = new List<Menu>();
+        public List<int>? MenusIds { get; set; } = new List<int>();
+        public List<int>? SmoothiesIds { get; set; } = new List<int>();
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if ((MenusIds == null || !MenusIds.Any()) &&
+            (SmoothiesIds == null || !SmoothiesIds.Any()))
+            {
+                yield return new ValidationResult(
+                    "You must select at least one menu or one smoothie.",
+                    new[] { nameof(MenusIds), nameof(SmoothiesIds) });
+            }
+        }
+
+        public IEnumerable<Customer> Customers { get; set; } = new List<Customer>();
+        public IEnumerable<int> SelectedMenusIds { get; set; } = new List<int>();
+        public IEnumerable<OrderSmoothie> OrderssSmoothies { get; set; } = new List<OrderSmoothie> { };
+        public IEnumerable<int> SelectedSmoothiesIds { get; set; } = new List<int>();
+        public IEnumerable<MenuOrder> MenusOrders { get; set; } = new List<MenuOrder> { };
+        public List<int>? CustomersIds { get; set; } = new List<int>();
+
     }
 }
