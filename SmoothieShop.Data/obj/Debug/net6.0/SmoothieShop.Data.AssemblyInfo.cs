483d6497defe67377ee0a3b92062daae84d09fab
IvannaGotsova <ivannagotsova@outlook.com>
2025-12-17 05:06:31 +0200

    Added Customer select menu in view

--- SmoothieShop.Data/obj/Debug/net6.0/SmoothieShop.Data.AssemblyInfo.cs
+++ SmoothieShop.Data/obj/Debug/net6.0/SmoothieShop.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmoothieShop.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93ff1a3d19f9097809f9dbc0a443b5ddc592ddea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8064b29ae9401559683714cfa0f444c9d0f94a93")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmoothieShop.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmoothieShop.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
