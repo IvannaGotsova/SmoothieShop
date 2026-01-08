d1ae9791cb39689cac45705743e99774262466bc
IvannaGotsova <ivannagotsova@outlook.com>
2026-01-08 11:26:11 +0200

    Modified Menu Model

--- SmoothieShop.Data/Models/MenuModels/AllMenusModel.cs
+++ SmoothieShop.Data/Models/MenuModels/AllMenusModel.cs
@@ -26,13 +26,7 @@ namespace SmoothieShop.Data.Models.MenuModels
         [Required]
         [Range(typeof(double), "0.00", "10000.00", ConvertValueInInvariantCulture = true)]
         public double Calories { get; set; }
-        [Required]
-        public int ProductUserId { get; set; }
-        [ForeignKey(nameof(ProductUserId))]
-        public ProductUser? ProductUser { get; set; }
-        [Required]
         public IEnumerable<Smoothie> Smoothies { get; set; } = new List<Smoothie>();
-        [Required]
         public IEnumerable<Order> Orders { get; set; } = new List<Order> { };
     }
 }
