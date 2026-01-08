ba54083ff6d4dd652fd18947fcc7d8b379ecebce
IvannaGotsova <ivannagotsova@outlook.com>
2026-01-08 11:29:37 +0200

    Modified new functionality

--- SmoothieShop.Core/Contracts/IMenuService.cs
+++ SmoothieShop.Core/Contracts/IMenuService.cs
@@ -72,5 +72,7 @@ namespace SmoothieShop.Core.Contracts
         Task<IEnumerable<Smoothie>> GetSmoothiesByMenu(int menuId);
         Task<IEnumerable<Order>> GetOrdersByMenu(int menuId);
         Task<IEnumerable<int>> GetSmoothiesIdsByMenu(int menuId);
+
+        Task<IEnumerable<Menu>> GetAllMenusByCustomer(int customerId);
     }
 }
