fe2e78740b2d94b6c68b32765b17aadd1aebe0d1
IvannaGotsova <ivannagotsova@outlook.com>
2026-01-09 07:34:28 +0200

    Added comments, removed unnecessary code, arrange files - Error Constants

--- SmoothieShop/ErrorConstants/ErrorConstants.cs
+++ SmoothieShop/ErrorConstants/ErrorConstants.cs
@@ -4,13 +4,17 @@
     /// This class holds Error constants messages.
     /// </summary>
    public static class ErrorConstants
-
    {
+        /// <summary>
+        /// This class holds Global Error constants messages.
+        /// </summary>
         public static class GlobalErrorConstants
         {
             public const string somethingWrong = "Something went wrong, please try again!";
         }
-
+        /// <summary>
+        /// This class holds User Error constants messages.
+        /// </summary>
         public static class UserErrorConstants
         {
             public const string invalidLogin = "Invalid Login";
