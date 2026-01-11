94255cafb8141762c76789ab1069361645433db0
IvannaGotsova <ivannagotsova@outlook.com>
2026-01-11 10:52:56 +0200

    Added comments, removed unnecessary code, arrange files - Models

--- SmoothieShop.Data/Models/FeedbackModels/AddFeedbackModel.cs
+++ SmoothieShop.Data/Models/FeedbackModels/AddFeedbackModel.cs
@@ -1,11 +1,6 @@
-﻿using SmoothieShop.Data.Data.Entites;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using SmoothieShop.Data.Data.Entites;
 using static SmoothieShop.Data.Data.DataConstants.DataConstants.FeedbackConstants;
 
 namespace SmoothieShop.Data.Models.FeedbackModels
--- SmoothieShop.Data/Models/FeedbackModels/DetailsFeedbackModel.cs
+++ SmoothieShop.Data/Models/FeedbackModels/DetailsFeedbackModel.cs
@@ -1,11 +1,6 @@
-﻿using SmoothieShop.Data.Data.Entites;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using SmoothieShop.Data.Data.Entites;
 using static SmoothieShop.Data.Data.DataConstants.DataConstants.FeedbackConstants;
 
 namespace SmoothieShop.Data.Models.FeedbackModels
@@ -24,10 +19,10 @@ namespace SmoothieShop.Data.Models.FeedbackModels
         [StringLength(FeedbackMaxLengthComment, MinimumLength = FeedbackMinLengthComment)]
         public string? Comment { get; set; }
         [Required]
+        public string? FeedbackUserName { get; set; }
+        [Required]
         public int CustomerId { get; set; }
         [ForeignKey(nameof(CustomerId))]
         public Customer? Customer { get; set; }
-        [Required]
-        public string? FeedbackUserName { get; set; }
     }
 }
