bd2cacd79136264ef30cf5d6a938aa418b4b285a
IvannaGotsova <ivannagotsova@outlook.com>
2026-01-12 12:03:46 +0200

    Added comments, removed unnecessary code, arrange files - DbContext

--- SmoothieShop.Data/SmoothieShopDbContext.cs
+++ SmoothieShop.Data/SmoothieShopDbContext.cs
@@ -2,8 +2,6 @@
 using Microsoft.EntityFrameworkCore;
 using SmoothieShop.Data.Data.Entites;
 using SmoothieShop.Data.Seeds;
-using System.Reflection.Emit;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace SmoothieShop.Data
 {
@@ -99,12 +97,6 @@ namespace SmoothieShop.Data
                 .Entity<MenuOrder>()
                 .HasKey(mo => new { mo.MenuId, mo.OrderId });
 
-            builder.Entity<MenuOrder>()
-    .HasOne(mo => mo.Order)
-    .WithMany(o => o.MenusOrders)
-    .HasForeignKey(mo => mo.OrderId)
-    .OnDelete(DeleteBehavior.Cascade);
-
             builder
                 .Entity<MenuSmoothie>()
                 .HasKey(ms => new { ms.MenuId, ms.SmoothieId });
@@ -117,11 +109,17 @@ namespace SmoothieShop.Data
                .Entity<OrderSmoothie>()
                .HasKey(os => new { os.OrderId, os.SmoothieId });
 
+            builder.Entity<MenuOrder>()
+                .HasOne(mo => mo.Order)
+                .WithMany(o => o.MenusOrders)
+                .HasForeignKey(mo => mo.OrderId)
+                .OnDelete(DeleteBehavior.Cascade);
+
             builder.Entity<OrderSmoothie>()
-    .HasOne(mo => mo.Order)
-    .WithMany(o => o.OrdersSmoothies)
-    .HasForeignKey(mo => mo.OrderId)
-    .OnDelete(DeleteBehavior.Cascade);
+               .HasOne(mo => mo.Order)
+               .WithMany(o => o.OrdersSmoothies)
+               .HasForeignKey(mo => mo.OrderId)
+               .OnDelete(DeleteBehavior.Cascade);
 
             /// <summary>
             /// Holds Configurations of the app.
