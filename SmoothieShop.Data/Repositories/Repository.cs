c9e7986f38b53cfe5c2c52f9b83142e5f8c7ccc1
IvannaGotsova <ivannagotsova@outlook.com>
2026-01-12 12:02:59 +0200

    Added comments, removed unnecessary code, arrange files - Repository

--- SmoothieShop.Data/Repositories/Repository.cs
+++ SmoothieShop.Data/Repositories/Repository.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SmoothieShop.Data.Repositories
 {
@@ -14,7 +9,6 @@ namespace SmoothieShop.Data.Repositories
     /// </summary>
     public class Repository : IRepository
     {
-
         protected DbContext Context { get; set; }
 
         protected DbSet<T> DbSet<T>() where T : class
@@ -26,56 +20,105 @@ namespace SmoothieShop.Data.Repositories
         {
             Context = context;
         }
-
+        /// <summary>
+        /// This method Add Async
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="entity"></param>
+        /// <returns></returns>
         public async Task AddAsync<T>(T entity) where T : class
         {
             await DbSet<T>().AddAsync(entity);
         }
-
+        /// <summary>
+        /// This method Add Range Async
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="entities"></param>
+        /// <returns></returns>
         public async Task AddRangeAsync<T>(IEnumerable<T> entities) where T : class
         {
             await DbSet<T>().AddRangeAsync(entities);
         }
-
+        /// <summary>
+        /// This method returns All
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         public IQueryable<T> All<T>() where T : class
         {
             return DbSet<T>().AsQueryable();
         }
-
+        /// <summary>
+        /// This method returns All
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="search"></param>
+        /// <returns></returns>
         public IQueryable<T> All<T>(Expression<Func<T, bool>> search) where T : class
         {
             return this.DbSet<T>().Where(search);
         }
-
+        /// <summary>
+        /// This method returns All Readonly
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         public IQueryable<T> AllReadonly<T>() where T : class
         {
             return this.DbSet<T>()
                 .AsNoTracking();
         }
+        /// <summary>
+        /// This method returns All Readonly
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="search"></param>
+        /// <returns></returns>
         public IQueryable<T> AllReadonly<T>(Expression<Func<T, bool>> search) where T : class
         {
             return this.DbSet<T>()
                 .Where(search)
                 .AsNoTracking();
         }
+        /// <summary>
+        /// This method Update Async
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public async Task UpdateAsync<T>(object id) where T : class
         {
             T entity = await GetByIdAsync<T>(id);
 
             Update<T>(entity);
         }
+        /// <summary>
+        /// This method Delete Async
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public async Task DeleteAsync<T>(object id) where T : class
         {
             T entity = await GetByIdAsync<T>(id);
 
             Delete<T>(entity);
         }
-
+        /// <summary>
+        /// This method Update
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="entity"></param>
         public void Update<T>(T entity) where T : class
         {
             this.DbSet<T>().Update(entity);
         }
-
+        /// <summary>
+        /// This method Delete
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="entity"></param>
         public void Delete<T>(T entity) where T : class
         {
             EntityEntry entry = this.Context.Entry(entity);
@@ -87,51 +130,85 @@ namespace SmoothieShop.Data.Repositories
 
             entry.State = EntityState.Deleted;
         }
-
+        /// <summary>
+        /// This method Detach
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="entity"></param>
         public void Detach<T>(T entity) where T : class
         {
             EntityEntry entry = this.Context.Entry(entity);
 
             entry.State = EntityState.Detached;
         }
-
+        /// <summary>
+        /// This method Dispose
+        /// </summary>
         public void Dispose() => this.Context.Dispose();
-
+        /// <summary>
+        /// This method Get by Id Async
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public async Task<T> GetByIdAsync<T>(object id) where T : class
         {
             return await DbSet<T>().FindAsync(id);
         }
-
+        /// <summary>
+        /// This method Get by Ids Async
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public async Task<T> GetByIdsAsync<T>(object[] id) where T : class
         {
             return await DbSet<T>().FindAsync(id);
         }
-
+        /// <summary>
+        /// This method Save Changes Async
+        /// </summary>
+        /// <returns></returns>
         public async Task<int> SaveChangesAsync()
         {
             return await this.Context.SaveChangesAsync();
         }
-
+        /// <summary>
+        /// This method Update Range
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="entities"></param>
         public void UpdateRange<T>(IEnumerable<T> entities) where T : class
         {
             this.DbSet<T>().UpdateRange(entities);
         }
-
+        /// <summary>
+        /// This method Delete Range
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="entities"></param>
         public void DeleteRange<T>(IEnumerable<T> entities) where T : class
         {
             this.DbSet<T>().RemoveRange(entities);
         }
-
+        /// <summary>
+        /// This method Delete Range
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="deleteWhereClause"></param>
         public void DeleteRange<T>(Expression<Func<T, bool>> deleteWhereClause) where T : class
         {
             var entities = All<T>(deleteWhereClause);
             DeleteRange(entities);
         }
-
+        /// <summary>
+        /// This method Remove Range
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="entities"></param>
         public void RemoveRange<TEntity>(IEnumerable<TEntity> entities) where TEntity : class
         {
             Context.Set<TEntity>().RemoveRange(entities);
         }
-
     }
 }
